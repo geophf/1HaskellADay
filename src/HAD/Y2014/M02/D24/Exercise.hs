58429083df0293525f333d96dec462ba847d74eb
Nicolas Biri <nicolas@biri.name>
2014-02-27 20:39:37 +0100

    Add 2014-02-24 exercise

---
+++ src/HAD/Y2014/M02/D24/Exercise.hs
@@ -0,0 +1,14 @@
+module HAD.Y2014.M02.D24.Exercise where
+
+-- | Filter a list, keeping an element only if it is equal to the next one.
+--
+-- Examples:
+-- >>> filterByPair []
+-- []
+-- >>> filterByPair [1 .. 10]
+-- []
+-- >>> filterByPair [1, 2, 2, 2, 3, 3, 4]
+-- [2,2,3]
+
+--filterByPair :: Find the most generic signature
+filterByPair = undefined
