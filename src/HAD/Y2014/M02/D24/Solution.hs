0296e7a996bfa548bff9551348df5da4ebf69c36
Nicolas Biri <nicolas@biri.name>
2014-02-27 20:40:00 +0100

    Add 2014-02-24 solution

---
+++ src/HAD/Y2014/M02/D24/Solution.hs
@@ -0,0 +1,15 @@
+module HAD.Y2014.M02.D24.Solution where
+
+import Data.List (group)
+
+-- | Filter a list, keeping an element only if it is equal to the next one.
+--
+-- Examples:
+-- >>> filterByPair []
+-- []
+-- >>> filterByPair [1 .. 10]
+-- []
+-- >>> filterByPair [1, 2, 2, 2, 3, 3, 4]
+-- [2,2,3]
+filterByPair :: Eq a => [a] -> [a]
+filterByPair = (tail =<<) . group
