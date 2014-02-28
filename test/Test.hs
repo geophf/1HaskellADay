0d021120e580137879ced1f5919943ab42052a5f
Nicolas Biri <biri@lippmann.lu>
2014-02-28 11:34:40 +0100

    Test can now open a problem directly from ghci, can check solution

--- test/Test.hs
+++ test/Test.hs
@@ -1,13 +1,66 @@
 module Test 
-  ( checkModule
-  , check
+  ( check
+  , checkSolution
   , checkCurrent
+  , edit
+  , editCurrent
   )
   where
 
+import Data.List (intercalate)
 import Data.Time
 import Text.Printf
 import Test.DocTest
+import System.FilePath.Posix (joinPath)
+import System.Process (readProcess)
+
+-- Runs the doctest of Exercise.hs file of te given date module.
+-- Example:
+-- 
+-- checkByDate 2014 02 24
+check :: Int -> Int -> Int -> IO ()
+check = checkFile "Exercise"
+
+-- Check the solution of a given Day
+checkSolution :: Int -> Int -> Int -> IO ()
+checkSolution = checkFile "Solution"
+
+-- Runs the doctest of today's Exercise.hs file.
+-- Example:
+-- 
+-- checkByDate 2014 02 24
+checkCurrent :: IO ()
+checkCurrent = runToday check
+
+
+-- Edit the exercise of a given day
+edit :: String -- Editor
+     -> Int -- Year of the exercise
+     -> Int -- Month
+     -> Int -- Day
+     -> IO ()
+edit ed y m d = do
+  let completePath = ("src":) . (++ ["Exercise.hs"]) $ modules y m d
+  flip (readProcess ed) [] $ return . joinPath $ completePath
+  return ()
+
+-- Edit today's exercise
+editCurrent :: String -- Editor
+            -> IO ()
+editCurrent = runToday . edit
+
+
+-- helper to run a function on the given day exercise
+runToday :: (Int -> Int -> Int -> IO ()) -> IO ()
+runToday f = do
+  (y, m, d) <- getCurrentDay
+  f (fromInteger y) m d
+
+-- Check the given file in the directory that corespond to the given day
+checkFile :: String -> Int -> Int -> Int -> IO ()
+checkFile fn y m d = let
+  modules2module = intercalate "." . (++ [fn])
+  in checkModule . modules2module $ modules y m d
 
 -- Runs the doctest of the Exercise.hs file of a given module.
 -- A bit painful to write, it's easier to use checkByDate.
@@ -17,24 +70,10 @@ import Test.DocTest
 checkModule :: String -> IO ()
 checkModule = doctest . ("-isrc":) . return
 
-
--- Runs the doctest of Exercise.hs file of te given date module.
--- Example:
--- 
--- checkByDate 2014 02 24
-check :: Int -> Int -> Int -> IO ()
-check = let buildModule = printf "HAD.Y%4d.M%02d.D%02d.Exercise"
-  in ((checkModule .) .) . buildModule
-
-
--- Runs the doctest of today's Exercise.hs file.
--- Example:
--- 
--- checkByDate 2014 02 24
-checkCurrent :: IO ()
-checkCurrent = do
-  (y, m, d) <- getCurrentDay
-  check (fromInteger y) m d
-
+-- Helper that get current Day
 getCurrentDay :: IO (Integer, Int, Int)
 getCurrentDay = fmap (toGregorian . utctDay) getCurrentTime
+
+-- helper that build modules list
+modules :: Int -> Int -> Int -> [String]
+modules y m d = "HAD": zipWith printf ["Y%4d", "M%02d", "D%02d"] [y,m,d]
