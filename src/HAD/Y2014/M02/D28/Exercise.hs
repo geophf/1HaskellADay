db847fbd15eeaddab1277a2a3e6d5bd601a45993
1HaskellADay <haskell@biri.name>
2014-02-28 13:35:13 +0100

    Add 2014-02-28 exercise

---
+++ src/HAD/Y2014/M02/D28/Exercise.hs
@@ -0,0 +1,14 @@
+module HAD.Y2014.M02.D28.Exercise where
+
+-- | use elements of the first lists to zip elements
+-- of the second and third lists
+--
+-- Examples
+-- >>> zipBinary [(+), (*), (*)] [2,2,2] [4,4,4]
+-- [6,8,8]
+-- >>> zipBinary [(+)] [2,2,2] [4,4,4]
+-- [6]
+-- >>> zipBinary (cycle [(+), (*)]) [1 .. 4] [2..5]
+-- [3, 6, 7, 20]
+zipBinary :: [a->b->c] -> [a] -> [b] -> [c]
+zipBinary ops xs ys = undefined
