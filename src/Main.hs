507e6cb9ba7ef1c93e59d77d55d7d3e1b1592924
1HaskellADay <haskell@biri.name>
2014-03-02 19:32:59 +0100

    Cleaning command parsing

--- src/Main.hs
+++ src/Main.hs
@@ -21,8 +21,8 @@ data Command
   | ReadCurrent
   | ReadSolution Int Int Int
   | Edit String Int Int Int
-  | InvalidCommand String
   | Help
+  | InvalidCommand
   deriving Eq
 
 readInt :: String -> Maybe Int
@@ -30,46 +30,50 @@ readInt s = case readsPrec 1 s of
   [(x,"")] -> Just x
   []       -> Nothing
 
-readDateCommand :: (Int -> Int -> Int -> Command) -> [String] -> Maybe Command
-readDateCommand cmd [xs,ys,zs] = do
+readDateCommand :: (Int -> Int -> Int -> Command)
+                -> String
+                -> String
+                -> String
+                -> Maybe Command
+readDateCommand cmd xs ys zs = do
     y <- readInt xs
     m <- readInt ys
     d <- readInt zs
     return $ cmd y m d
-readDateCommand cmd _ = Nothing
+
+readCommandName :: String -> (Int -> Int -> Int -> Command)
+readCommandName "check" = Check
+readCommandName "read"  = Read
+readCommandName "checkSolution" = CheckSolution
+readCommandName "readSolution"  = ReadSolution
 
 checkDateCommand :: Maybe Command -> Command
-checkDateCommand = fromMaybe $ InvalidCommand notice
+checkDateCommand = fromMaybe $ InvalidCommand
     
 
 readCommand :: [String] -> Command
 readCommand ["check","current"] = CheckCurrent
-readCommand ("check":xs) =
-  checkDateCommand $ readDateCommand Check xs
-readCommand ("checkSolution":xs) =
-  checkDateCommand $ readDateCommand CheckSolution xs
 readCommand ["read","current"] = ReadCurrent
-readCommand ("read":xs) = checkDateCommand $ readDateCommand Read xs
-readCommand ("readSolution":xs) =
-  checkDateCommand $ readDateCommand ReadSolution xs
-readCommand ("edit":ed:xs) =
-  checkDateCommand $ readDateCommand (Edit ed) xs
 readCommand ("help":_) = Help
-readCommand _ = InvalidCommand $ "unknown command\n\n" ++ notice
+readCommand [cmd,y,m,d] =
+  checkDateCommand $ readDateCommand (readCommandName cmd) y m d
+readCommand ["edit",ed,y,m,d] =
+  checkDateCommand $ readDateCommand (Edit ed) y m d
+readCommand _ = InvalidCommand
 
 instance Read Command where
   readsPrec _ = return . (id &&& const "") . readCommand . words
 
 execCommand :: Command -> IO ()
-execCommand (Check y m d) = check y m d
 execCommand CheckCurrent = checkCurrent
-execCommand (CheckSolution y m d) = checkSolution y m d
-execCommand (Read y m d) = readExercise y m d
 execCommand ReadCurrent = readCurrentExercise
+execCommand (Check y m d) = check y m d
+execCommand (Read y m d) = readExercise y m d
+execCommand (CheckSolution y m d) = checkSolution y m d
 execCommand (ReadSolution y m d) = readSolution y m d
-execCommand (InvalidCommand s) = putStrLn s
 execCommand (Edit e y m d) = edit e y m d
 execCommand Help = putStrLn notice
+execCommand InvalidCommand = putStrLn ("Invalid command\n\n" ++ notice)
 
 notice = unlines
   [ "usage: 1had <command> [<args>]"
