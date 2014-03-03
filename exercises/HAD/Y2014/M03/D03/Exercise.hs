96960874d686bced7d70c4e5dec8b3dc30ff89e8
1HaskellADay <haskell@biri.name>
2014-03-03 09:11:02 +0100

    Add 2014-03-03 exercise

---
+++ exercises/HAD/Y2014/M03/D03/Exercise.hs
@@ -0,0 +1,19 @@
+module HAD.Y2014.M03.D03.Exercise where
+
+-- | Sort a list of list of elements by the maximum of each list,
+-- in ascending order
+-- 
+-- Point-free: easy and readable
+-- Level: EASY
+--
+-- Examples:
+-- >>> sortByMax [[1,10],[5,5]]
+-- [[5,5],[1,10]]
+-- >>> sortByMax []
+-- []
+-- 
+-- sortByMax [[], [1,2]]
+-- should throw an execption: no max for empty list
+
+-- sortByMax :: Find the most generic signature
+sortByMax = undefined
