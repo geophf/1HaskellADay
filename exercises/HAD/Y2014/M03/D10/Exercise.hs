376e9e418ea8649d20090370e4f045d2c0919faf
1HaskellADay <haskell@biri.name>
2014-03-10 17:25:21 +0100

    Add 2014-03-10 exercise

---
+++ exercises/HAD/Y2014/M03/D10/Exercise.hs
@@ -0,0 +1,23 @@
+module HAD.Y2014.M03.D10.Exercise where
+
+-- $setup
+-- >>> import Test.QuickCheck
+-- >>> import Control.Applicative
+
+-- | maybeReadPositiveInt Try to parse a positive Int
+-- Can be done point-free (and it's probably funnier this way).
+--
+-- Examples:
+--
+-- prop> (==) <$> Just <*> maybeReadPositiveInt . show $ getNonNegative x
+--
+-- prop> Nothing == (maybeReadPositiveInt . show . negate . getPositive $ x)
+--
+-- >>> maybeReadPositiveInt "foo"
+-- Nothing
+--
+-- >>> maybeReadPositiveInt "12 "
+-- Nothing
+--
+maybeReadPositiveInt :: String -> Maybe Int
+maybeReadPositiveInt = undefined
