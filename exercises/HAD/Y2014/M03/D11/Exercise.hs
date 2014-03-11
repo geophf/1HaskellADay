df8dbb13158103ee777900f5b6fbf680250545d0
1HaskellADay <haskell@biri.name>
2014-03-11 11:26:57 +0100

    Add 2014-03-11 exercise

---
+++ exercises/HAD/Y2014/M03/D11/Exercise.hs
@@ -0,0 +1,25 @@
+module HAD.Y2014.M03.D11.Exercise where
+
+-- | lcAlphabetFrom
+-- Display the alaphabet in lower cas, starting from the letter given in
+-- parameter.
+-- If the parameter is not a lowercase letter, displays the alphabet from 'a'
+--
+-- Point-free is quite easy
+--
+-- Examples:
+--
+-- >>> lcAlphabetFrom 'a'
+-- "abcdefghijklmnopqrstuvwxyz"
+--
+-- >>> lcAlphabetFrom 'e'
+-- "efghijklmnopqrstuvwxyzabcd"
+--
+-- >>> lcAlphabetFrom '`'
+-- "abcdefghijklmnopqrstuvwxyz"
+--
+-- >>> lcAlphabetFrom '{'
+-- "abcdefghijklmnopqrstuvwxyz"
+
+lcAlphabetFrom :: Char -> String
+lcAlphabetFrom = undefined
