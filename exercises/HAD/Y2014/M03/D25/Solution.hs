16db4480af63741453300bc97c51a60e52f01716
1HaskellADay <haskell@biri.name>
2014-03-25 16:54:33 +0100

    Add 2014-03-25/26 content

---
+++ exercises/HAD/Y2014/M03/D25/Solution.hs
@@ -0,0 +1,41 @@
+module HAD.Y2014.M03.D25.Solution
+  ( Board
+  , board
+  , getList
+  ) where
+
+import Data.List (groupBy)
+import Control.Applicative ((<*>))
+import Control.Monad (liftM, replicateM)
+
+import Test.QuickCheck
+
+-- Preamble
+
+-- $setup
+-- >>> import Control.Applicative ((<$>), (<*>))
+-- >>> import Data.List (group)
+
+-- A board is a "square list of list"
+-- The "square" form, is ensure by the constructor (board)
+newtype Board a = Board {getList :: [[a]]}
+  deriving (Eq, Show, Read)
+
+-- | Exercise
+-- Build an Arbitrary instance for Board of any given size
+--
+-- The external list length is equal to the length of each internal lists
+-- prop> (1==) . length . group . (flip (:) <*> length) . map length $ getList bs
+--
+instance Arbitrary a => Arbitrary (Board a) where 
+  arbitrary = liftM Board (arbitrary >>= replicateM <*> vector)
+
+
+
+-- Just some extra content, it isn't useful for today's exercise
+
+-- Constructor
+
+-- | board Yesterday's squareOf, build a square board with initial values
+board :: Int -> a -> [a] -> [[a]] 
+board n x = take n . map (take n) . iterate (drop n) . (++ repeat x)
---
+++ exercises/HAD/Y2014/M03/D26/Exercise.hs
@@ -0,0 +1,69 @@
+module HAD.Y2014.M03.D26.Exercise
+  ( Board
+  , board
+  , getList
+  , Direction (..)
+  , viewFrom
+  ) where
+
+import Data.List (groupBy)
+import Control.Applicative ((<*>))
+import Control.Monad (liftM, replicateM)
+
+import Test.QuickCheck
+
+-- Preamble
+
+-- $setup
+-- >>> import Control.Applicative ((<$>), (<*>))
+-- >>> import Data.List (sort)
+-- >>> :{
+--   let checkReverse d1 d2 =
+--     (==) <$>
+--        sort . map sort . getList . viewFrom d1 <*>
+--        sort . map (sort . reverse) . getList . viewFrom d2 
+-- :}
+
+newtype Board a = Board {getList :: [[a]]}
+  deriving (Eq, Show)
+
+data Direction = North | South | East | West
+  deriving (Eq, Read, Show)
+
+-- Exercise
+
+-- | viewFrom given a direction, produce an involution such that the
+-- inner lists elements are ordered as if they were seen from that direction.
+-- 
+--
+-- Examples: 
+--
+-- Defaut view is from West
+-- prop> xs == viewFrom West xs
+--
+-- The function is an involution
+-- prop> \(d,xxs) -> (==) <*> (viewFrom d . viewFrom d) $ (xxs :: Board Int)
+--
+-- Ordering properties from opposite side views (for inner lists elements
+-- prop> checkReverse West  East  (xxs :: Board Int)
+-- prop> checkReverse East  West  (xxs :: Board Int)
+-- prop> checkReverse North South (xxs :: Board Int)
+-- prop> checkReverse South North (xxs :: Board Int)
+--
+viewFrom :: Direction -> Board a -> Board a
+viewFrom = undefined
+
+
+-- Constructor
+
+-- | board Yesterday's squareOf, build a square board with initial values
+board :: Int -> a -> [a] -> [[a]] 
+board n x = take n . map (take n) . iterate (drop n) . (++ repeat x)
+
+-- Arbitrary instances
+
+instance Arbitrary a => Arbitrary (Board a) where 
+  arbitrary = liftM Board (arbitrary >>= replicateM <*> vector)
+
+instance Arbitrary Direction where
+  arbitrary = elements [North, South , East , West]
