801dbcebe80fe308dc9e1ee398d3553974c302e7
1HaskellADay <haskell@biri.name>
2014-04-12 05:10:58 +0200

    Add 2014-04-10 content

---
+++ exercises/HAD/Y2014/M04/D10/Solution.hs
@@ -0,0 +1,20 @@
+module HAD.Y2014.M04.D10.Solution where
+
+import Control.Monad (join)
+
+{- | onBothSide
+   Apply the same argument on both side of a binary numeric function.
+
+   The only interesting solution is point-free.
+
+   It IS the easiest exercise so far, with the shortest solution.
+
+   Examples:
+
+   prop> onBothSide (+) x = x + x
+
+   prop> onBothSide (*) x = x * x
+
+-}
+onBothSide :: Num a => (a -> a -> b) -> a -> b
+onBothSide = join
