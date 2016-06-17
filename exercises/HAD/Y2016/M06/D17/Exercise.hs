9ef8ec8921323896571f9aa1c0712c7c7cc61b4d
geophf <dauclair@hotmail.com>
2016-06-17 17:47:52 -0400

    Added exercise to draw x/y-axes

---
+++ exercises/HAD/Y2016/M06/D17/Exercise.hs
@@ -0,0 +1,28 @@
+import Analytics.Trading.Data.Row     -- http://lpaste.net/109658
+
+import Data.Graphics.SVG
+import Data.XHTML (Element)
+
+{--
+Yesterday we generated a candlestick then a set of candlesticks over a set
+of historic prices of a stock (TWTR). Well, great, but what does it all mean?
+
+The eternal question.
+
+So! Let's provide the context.
+
+From yesterday's data set, determine the highest high and the lowest low for
+a sense of scale and then, today, what we will do is provide a grid of 
+x and y axes labeled with lowest and highest values for each (you can get 
+fancier if you'd like) and THEN!
+
+... well, and then we're done with today's problem.
+
+Simple!
+
+The data is at twtr.csv under yesterday's exercise.
+--}
+
+axes :: [Row] -> Element    -- from a set of Rows, give labeled axes in which
+                            -- to chart the data
+axes = undefined
