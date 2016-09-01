3885d5fdd547a04c3036765f67ce30fa62a371e4
geophf <dauclair@hotmail.com>
2016-09-01 15:32:42 -0400

    Updated hashing for data: hashing data, then hashing the hash, to conform to bitcoin's approach

--- exercises/HAD/Data/Tree/Merkle.hs
+++ exercises/HAD/Data/Tree/Merkle.hs
@@ -11,13 +11,18 @@ import Control.Logic.Frege (adjoin)
 {--
 We'll look at Merkle trees from the tweet we saw yesterday on @1HaskellADay
 from Carlos Galdino @carlosgaldino.
+
 http://blog.carlosgaldino.com/merkle-trees.html
+
 Also, Merkle trees are used in the Blockchain (which BitCoin uses), so here's
 an article on that.
+
 http://chimera.labs.oreilly.com/books/1234000001802/ch07.html#merkle_trees
+
 We're not going to declare and construct Merkle trees today, what we are
 going to do is to get warmed up with hashing functions, specifically the
 SHA256 hashing function.
+
 Define a function that takes a string and hashes it to an SHA256 digest:
 --}
 
@@ -25,6 +30,10 @@ hashShow :: Show a => a -> Digest SHA256
 hashShow = hashlazy . BL.pack . show
 
 -- actually, hashShow works on anything that has a String-representation.
+-- And we want that hash to be doublely hashed so:
+
+hashDatum :: Show a => a -> Digest SHA256
+hashDatum = hashShow . show . hashShow
 
 -- Hash the following strings:
 
@@ -34,9 +43,9 @@ hashme = ["I like to move it, move it!", "π is 3.14159265358979323846..."]
 -- what are the SHA256 hashes of the above strings?
 
 {--
-*Y2016.M08.D31.Solution> mapM_ (print . hashShow) hashme ~> hashes0
-df8c3ee1b1a5b4201cb2baf8df3cbc2e008189eba983a6ca2e296100e15a1e62
-0dd10591d26d65e6334a0d8eb08432757dcde511ef3fde01ab29daa1a8613302
+*Data.Tree.Merkle> mapM_ (print . hashDatum) hashme ~> hash0
+18602aedaaecb9629303add65c23e7a53753d672ac0150cea9c838ca3c69512f
+e08e9c734649decf1e099179d85b6b3e8fc7f0fdd7a2f8c4e09d1286e4a59838
 --}
 
 {--
@@ -55,9 +64,9 @@ hashhash = hashShow
 -- of a hash is not the original hash.
 
 {--
-*Y2016.M08.D31.Solution> mapM_ (print . hashhash) hashes0 ~> hashes1
-dd414391543bf5a9fc368a512a788e5776a9fcd23066800ac5d3bd2aab5da3c6
-8acff9a3032ef8657bea2f3893408dd55b54158502bbda8b86dc1f1b0b134b34
+*Y2016.M08.D31.Solution> mapM_ (print . hashhash) hash0 ~> hashes1
+f80961602c4ea1b17ede78ede6ea7d9ac43229dc70f77129dedf3f6e0786664e
+e236d4d55f3da96c7aa5ec8c4896622abe0ae13e2d2339edf71b04c4df040fdb
 
 verified by eyeballs, the hashes are different.
 --}
@@ -71,12 +80,12 @@ resulting String ... result.
 --}
 
 childrenHash :: Digest SHA256 -> Digest SHA256 -> Digest SHA256
-childrenHash = curry (adjoin show >>> uncurry (++) >>> hashShow >>> hashhash)
+childrenHash = curry (adjoin show >>> uncurry (++) >>> hashDatum)
 
 -- Take the two hashed hashes from the above strings, concatenate them, then
 -- hash hash that concatenation (of the two hash hashes). What is your result?
 
 {--
-*Y2016.M08.D31.Solution> uncurry childrenHash $ (head &&& last) hashes1 ~>
-4b222f0abfc954557b985d8b0dce3800225285755b0102bed06e1993dd584226
+*Data.Tree.Merkle> uncurry childrenHash $ (head &&& last) hash0
+90df6361bb3159f83c4b0554d5cf2331a6826b6cd0fe8ad83c61d80569e39a86
 --}
