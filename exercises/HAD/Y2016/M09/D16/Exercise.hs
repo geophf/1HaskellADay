b98f6e3c42c98bd8d574851459cf2c190797a552
geophf <dauclair@hotmail.com>
2016-09-16 09:43:45 -0400

    Sudoku with a twist: BINOCULAR!

---
+++ exercises/HAD/Y2016/M09/D16/Exercise.hs
@@ -0,0 +1,62 @@
+module Y2016.M09.D16.Exercise where
+
+-- below module is available from 1HaskellADay git repository
+
+import Data.Matrix
+
+{-- 
+Here's a Sudoku with a word-twist. 
+
+The below puzzle contains the word BINOCULAR in each row, column, and in each
+nonent (9 cells) with centers at
+--}
+
+nonentCenters :: [(Int, Int)]
+nonentCenters = [(r,c) | r <- centers, c <- centers]
+   where centers = [2,5,8]
+
+-- So, a nonent is the cells with a nonent-center (above) and the eight cells
+-- encircling that nonent-center. Define a nonent algorithmically:
+
+nonent :: (Int, Int) -> [(Int, Int)]
+nonent center = undefined
+
+     -- gives the cell indices of the nonent, including the given center index
+
+-- Okay, so here is the BINOCULAR sudoku. 
+
+data Letter = Ltr Char
+   deriving (Eq, Ord)
+
+instance Show Letter where
+   show (Ltr l) = pure l
+
+binocularPuzzle :: Matrix Letter
+binocularPuzzle =
+   fromLists 
+     (map (map Ltr) ["AU O  C  ","  RI    U"," I  U NB ","  UR I C ","IC     RN",
+                    " N U CL  "," AI O    ","B    UI  ","  C  L UO"])
+
+{--
+*Y2016.M09.D16.Exercise> pprint binocularPuzzle ~>
+Matrix 9x9
+| A U   O     C     |
+|     R I         U |
+|   I     U   N B   |
+|     U R   I   C   |
+| I C           R N |
+|   N   U   C L     |
+|   A I   O         |
+| B         U I     |
+|     C     L   U O |
+
+Solve it by replacing the spaces with the missing letters from the word
+--}
+
+word :: [Letter]
+word = map Ltr "BINOCULAR"
+
+-- *Y2016.M09.D16.Exercise> word ~> [B,I,N,O,C,U,L,A,R]
+
+-- given the standard rules of sudoku: no letter repeats in a row, column, nor
+-- nonent
