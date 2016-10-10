dc094a5b9019f5f6bff5306408d981e34a563d45
geophf <dauclair@hotmail.com>
2016-10-10 12:52:57 -0400

    Added Sheesh type-class for all hashed values of the block chain

--- exercises/HAD/Data/BlockChain/Block/Types.hs
+++ exercises/HAD/Data/BlockChain/Block/Types.hs
@@ -3,3 +3,14 @@ module Data.BlockChain.Block.Types where
 -- we keep the common types, used by BlockChain types, here
 
 type Hash = String
+
+-- MANY block chain types have a hashed value. It becomes onerous to come up
+-- with new names for each of these values so:
+
+class Sheesh a where
+   hash :: a -> Hash
+
+-- Name 'Sheesh' goes with hashed-types, see? hashSheesh!
+-- I'll leave now.
+
+-- Motivated to add type by @MadameRecluse
