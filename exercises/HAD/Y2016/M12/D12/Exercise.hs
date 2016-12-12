4c2fd76fb2f6fa627709218b29af0294414026e9
geophf <dauclair@hotmail.com>
2016-12-12 11:44:36 -0500

    A dinner-table conversation of a math professor and her husband

---
+++ exercises/HAD/Y2016/M12/D12/Exercise.hs
@@ -0,0 +1,20 @@
+module Y2016.M12.D12 where
+
+{--
+Today's Haskell exercise is from the Mensa Genuis Quiz-a-Day book. And I quote:
+
+"Aha," said the math professor to her husband, "it's a very simple problem, but
+I see that our two sons have now reached interesting ages. The product of their
+ages is six times the amount you get if you add their ages. On the other hand,
+if you add the square of each age, the total of the two will be 325." How old
+were their boys?
+
+Okay, solve that. ... How?
+
+Question: do you have conversations like this around the dinner table, ...
+like I do?
+--}
+
+twoBirthdayBoyz :: [(Int, Int)]
+twoBirthdayBoyz = undefined
+
