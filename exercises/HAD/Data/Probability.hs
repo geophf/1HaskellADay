83a8aef9f09d9259931db42179328b085608b82c
geophf <dauclair@hotmail.com>
2017-02-16 08:02:50 -0500

    Questions on probable outcomes of genotypes with the upgraded probability library

--- exercises/HAD/Data/Probability.hs
+++ exercises/HAD/Data/Probability.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE TupleSections #-}
+
 module Data.Probability where
 
-import Control.Arrow (first, second)
+import Control.Arrow (first, second, (***), (&&&))
+
+import Data.Function (on)
+import Data.List (groupBy, sort, genericLength)
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Ratio
 
 -- A direct lift from http://learnyouahaskell.com/for-a-few-monads-more#making-monads
@@ -30,16 +37,7 @@ instance Monad Prob where
 
 instance Applicative Prob where
    pure x = Prob [(x,1)]
-   (Prob fs) <*> (Prob xs) =
-
--- Okay, let's think this through
-
--- Okay, let's not. Let's let YOU think this through. AHA!
-
-      undefined
-
--- Yeah, I'm going to make this an @1HaskellADay exercise. That's right.
--- I went there.
+   Prob fs <*> Prob xs = Prob (map (\(f,p) -> f *** (p *)) fs <*> xs)
 
 -- From the above, with a loaded coin, we can compute the probabilities of
 -- all tails in a coin-toss:
@@ -56,4 +54,38 @@ flipThree = coin >>= \a -> coin >>= \b -> loadedCoin >>= \c ->
 
 -- Should be what?
 
--- Exercise, return Probabilities as a mapping
+-- Exercise, return Probabilities as a mapping. We do this with condense':
+
+condense' :: Ord a => Prob a -> Map a Rational
+condense' = Map.fromList . map (fst . head &&& foldr (+) 0 . map snd)
+          . groupBy ((==) `on` fst) . sort . getProb
+
+{--
+>>> flipThree
+Prob {getProb = [(False,1 % 40),(False,9 % 40),(False,1 % 40),(False,9 % 40),
+                 (False,1 % 40),(False,9 % 40),(False,1 % 40),(True,9 % 40)]}
+>>> condense' flipThree
+fromList [(False,31 % 40),(True,9 % 40)]
+--}
+
+-- now that you've got the condensate, recoalesce the result into a probability
+-- distribution
+
+recoalesce :: Ord a => Map a Rational -> Prob a
+recoalesce = Prob . Map.toList
+
+{--
+>>> recoalesce $ condense' flipThree
+Prob {getProb = [(False,31 % 40),(True,9 % 40)]}
+--}
+
+-- let's tie everything together into one function:
+
+condense :: Ord a => Prob a -> Prob a
+condense = recoalesce . condense'
+
+-- we also need to represent a uniform probability distribution, that is: for any
+-- value x in a probability distribution, any value occurs with equal likelihood
+
+uniform :: [a] -> Prob a
+uniform as = let prob = 1 % genericLength as in Prob (map (,prob) as)
---
+++ exercises/HAD/Y2017/M02/D16/Exercise.hs
@@ -0,0 +1,71 @@
+module Y2017.M02.D16.Exercise where
+
+import Data.Probability
+
+{--
+A Genotype is a paired value a trait dominance, so that for some trait, T,
+the genotype of a purely dominant trait is TT, mixed (showing dominant) is Tt
+and a purely recessive trait is tt.
+
+Note that a mixed trait Tt is always written thus (so Tt == tT, as it were).
+
+Declare a Genotype that takes Char values to represent the alleles.
+--}
+
+data Genotype = GenotypeDeclarationYouCreate
+   deriving (Eq, Ord, Show)
+
+{--
+Now, in conjugation, each parent gene donates either allele with equal 
+likelihood to the child gene.
+
+So, for example, you have two parents, one with genotype Aa, the other
+with genotype AA, the child has one of the following four possible genes:
+
+ Aa x AA = (AA, AA, Aa, Aa) or, simplifying: (AA, Aa) each option equally likely
+
+So, today's Haskell problem. Given a genotype, one from each parent, define
+the function, cross, that gives a probability distribution of the resulting
+genotype in the offspring.
+--}
+
+cross :: Genotype -> Genotype -> Prob Genotype
+cross a b = undefined
+
+-- What is the Genotype of a child whose parents are a = "Aa" b = "AA"?
+-- (as a probability distribution)
+
+parentsA, parentsB :: (Genotype, Genotype)
+parentsA = undefined  -- Aa, AA
+
+-- What is the Genotype of a child whose parents are a = "Aa" b ="Aa"?
+
+parentsB = undefined  -- Aa, Aa
+
+-- Okay, what is the Genotype of the child of the children of parents A and B?
+-- That is to say, the grandchild of parentsA and parentsB?
+ 
+-- Hm, how would you define that, as the parents are now the children who
+-- are probability distributions. How do we call cross not with plain genotypes,
+-- but with probability distribuions?
+
+-- What is the probability that this grandchild is purely recessive? (aa)
+-- What is the probability that this grandchild is purely dominant? (AA)
+
+-- Taking a leaf from the flipThree example from Data.Probability:
+
+-- Let's say the parents have two children each (total of four children)
+-- And let's say those children marry (no incest) and let's say those new
+-- parents have two children each.
+
+-- 1. How many grandchildren are there?
+
+-- What is the probability that 1 or more of the children is mixed (Aa)?
+
+-- What is the probability that 2 or more of the children are mixed?
+
+-- Hint: the probability of x outcomes is what? (think: combinations), so
+-- the probability of at least x outcomes is x outcomes + x+1 outcomes + ...
+
+-- 2. Of course, organisms have more than just one genotype. Tomorrow we'll
+-- consider multiple genotypes.
