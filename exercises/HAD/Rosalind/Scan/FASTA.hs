95e510e8bb1ad5d416ad69d63056dacc2e2b7fed
geophf <dauclair@hotmail.com>
2017-02-16 08:14:10 -0500

    Simplified Scan.FASTA module to just the functions for reading in a FASTA file

--- exercises/HAD/Rosalind/Scan/FASTA.hs
+++ exercises/HAD/Rosalind/Scan/FASTA.hs
@@ -1,40 +1,22 @@
 module Rosalind.Scan.FASTA where
 
-import Data.Function (on)
-import Data.List (maximumBy)
-import qualified Data.Map as Map
-import Data.Monoid
-import Data.Ratio
-
--- below imports available from 1HaskellADay git repository
-
-import Control.Logic.Frege (adjoin, (-|))
-import Data.Bag (Bag)
-import qualified Data.Bag as Bag
-import Data.Percentage
-
 {--
-Computing GC Content, a solution to the problem posted at
-http://lpaste.net/116305
+The FASTA-file format is as follows:
 
-We intermingle a bit here: a bit of scanning, a bit of analysis, but
-if we want to read in a FASTA file, then readFASTA file
+>Rosalind_6404
+CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
+TCCCACTAATAATTCTGAGG
+>Rosalind_5959
+CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
+ATATCCATTTGTCAGCAGACACGC
 
-will get you what you need.
+... etc.
+
+We need to read in strands of sequenced DNA and tag them.
 --}
 
-type DNAStrand = String
-
-gcContent :: DNAStrand -> Percentage
-gcContent strand =
-   let gcs = getSum (sumGC (Map.toList (Bag.fromList strand)))
-   in  P (uncurry (%) $ adjoin fromIntegral (gcs, length strand))
-
-sumGC :: Monoid m => [(Char, m)] -> m
-sumGC = foldr (\(x,y) -> ((x `elem` "GC" -| y) <>)) mempty
--- sumGC ((x, y):rest) = (if x `elem` "GC" then y else 0) + sumGC rest
-
 type Ident = String
+type DNAStrand = String
 
 data IdxStrand = IS { ident :: Ident, strand :: DNAStrand }
    deriving (Eq, Ord, Show)
@@ -54,16 +36,6 @@ readIdxStrand (('>':rosy):rest) = return (IS rosy strand, remaining)
          grabStrand ans@(('>':_):_) str = (str, ans)
          grabStrand (x:y) str = grabStrand y (str ++ x)
 
-{--
-*Main> liftM (readIdxStrand . lines) (readFile "samp_strands_gc.txt" )
-[(IS "Rosalind_6404" ("CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGC"
-                      ++ "TTCCGGCCTTCCCTCCCACTAATAATTCTGAGG"),
-    [">Rosalind_5959","CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCT"
-                  ++ "CCGCCGAAGGTCT", "ATATCCATTTGTCAGCAGACACGC",
-     ">Rosalind_0808","CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTT"
-                  ++ "CAGACCAGCCCGGAC", "TGGGAACCTGCGGGCAGTAGGTGGAAT"])]
---}
-
 readStrands :: [String] -> [IdxStrand]
 readStrands = parseStrands . readIdxStrand
 
@@ -79,19 +51,4 @@ parseStrands ((strand, rest):_) = strand : readStrands rest
                      ++ "CCGAAGGTCTATATCCATTTGTCAGCAGACACGC"),
  IS "Rosalind_0808" ("CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACC"
                      ++ "AGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT")]
---}
-
-is2gc :: IdxStrand -> (Ident, Percentage)
-is2gc (IS name strand) = (name, gcContent strand)
-
-maxGcContent :: [IdxStrand] -> (Ident, Percentage)
-maxGcContent = maximumBy (compare `on` snd) . map is2gc
-
-{--
-*Main> liftM (maxGcContent . readStrands . lines) (readFile "rosy_strands.txt")
-("Rosalind_0808",60.91954022988506)
-*Main> liftM (maxGcContent . readStrands . lines) (readFile "rosalind_gc.txt")
-("Rosalind_5105",52.22222222222222)
-
-WOOT! I say!
 --}
