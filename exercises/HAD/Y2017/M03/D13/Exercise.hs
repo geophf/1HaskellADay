80e223aad21f2a8c6629c9359727274b98cb938d
geophf <dauclair@hotmail.com>
2017-03-13 11:32:44 -0400

    An Anagramatic poem

---
+++ exercises/HAD/Y2017/M03/D13/Exercise.hs
@@ -0,0 +1,35 @@
+module Y2017.M03.D13.Exercise where
+
+{--
+
+Oh, yeah. Let's do this one!
+
+https://twitter.com/HaggardHawks/status/840321681726013440
+
+Nic Wilkinson Retweeted
+Haggard Hawks @HaggardHawks  Mar 10
+All fourteen lines in David Shulman’s sonnet ‘Washington Crossing The Delaware’ 
+(1936) are anagrams of the title.
+
+prove it!
+--}
+
+anagram :: String -> String -> Bool
+anagram line = undefined
+
+-- snaps for defining anagram elegantly
+
+sonnet :: FilePath
+sonnet = "Y2017/M03/D13/sonnet.txt"
+
+{--
+So, read in the first line. Use it as the basis of the anagram. 
+
+For each line thereafter:
+
+Read in each line. Remove non-alpha characters. Determine if that line is an
+anagram of the first line.
+
+What are your results?
+--}
+
