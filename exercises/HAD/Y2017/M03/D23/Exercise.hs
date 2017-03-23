6c5f244dbeddef5a28f0b593f718dc6957d12069
geophf <dauclair@hotmail.com>
2017-03-22 21:55:08 -0400

    Red socks and black socks. But seriously: who wears red socks?

---
+++ exercises/HAD/Y2017/M03/D23/Exercise.hs
@@ -0,0 +1,39 @@
+module Y2017.M03.D21.Exercise where
+
+{--
+Okay, we're going explore a bit of probability and see where that takes us.
+
+These problems are from Fifty Challenging Problems in Probability with Solutions
+by Frederick Mosteller.
+
+Problem 1
+
+A drawer contains red socks and black socks. When two socks are drawn at random,
+the probability that both are red is 1/2.
+--}
+
+-- (a) How small can the number of socks in the drawer be?
+
+totalSocks :: Int
+totalSocks = undefined
+
+-- (b) How small if the number of black socks is even?
+
+totalSocksGivenEvenBlack :: Int
+totalSocksGivenEvenBlack = undefined
+
+{--
+Okay, serious question to you red-or-black sock owners.
+
+(1) Who has a drawer full of RED SOCKS? Is this some College professor-thing?
+
+(2) Who, after a week, hasn't figured out you put the RED SOCKS on the LEFT side
+    of the drawer and BLACK SOCKS on the RIGHT side of the drawer, so your 
+    probability of drawing two red socks is 100% because you're not a dummy?
+
+    It's called algebraic types, and it's good for more than just Haskell.
+    Just saying.
+
+    But no, that's okay: arrange your sock drawer in whichever way your heart 
+    desires.
+--}
