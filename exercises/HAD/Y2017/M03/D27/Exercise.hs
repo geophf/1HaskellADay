f74ef42ff2cce4ed3894b075b159daae5aed5e34
geophf <dauclair@hotmail.com>
2017-03-27 10:02:03 -0400

    Casting die: Luck, be a Lady tonight!

---
+++ exercises/HAD/Y2017/M03/D27/Exercise.hs
@@ -0,0 +1,19 @@
+module Y2017.M03.D27.Exercise where
+
+-- below import available from 1HaskellADay git repository
+
+import Data.Probability
+
+{--
+Simple one, from a probability-persective:
+
+Problem 4: Trials until First Success
+(from Fifty Challenging Problems in Probability by Frederick Mosteller)
+
+On the average, how many times must a die be thrown until one gets a 6?
+
+You might be able to use the above import to help answering this question.
+--}
+
+throwsUntil6 :: Int
+throwsUntil6 = undefined
