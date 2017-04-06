03b0949072d477f6f721157402310c91bf5d4051
geophf <dauclair@hotmail.com>
2017-04-06 10:05:33 -0400

    Today we resurrect CP/M because we miss the good, old days!

---
+++ exercises/HAD/Y2017/M04/D06/Exercise.hs
@@ -0,0 +1,50 @@
+module Y2017.M04.D06.Exercise where
+
+import System.Info
+
+{--
+So, different operating systems have different ways of addressing stored
+information. Windows, we all know, has the CP/M weirdo '\'-character relic that
+separates directories from directories from files. Unix/Linux/SCOix/Darwinish
+has the '/' for hierarchical naming of inodes, and zOS has '.' to address
+boundaries in the mainframe and MacOS 8 has '#' ... just because.
+
+Just because they 'think different' [sic].
+
+So, GIVEN that you know the operating system ... (HOW)? ... create an operator
+that returns the appropriate address-separating token to index to resources
+managed by the operating system.
+--}
+
+type OS = String
+
+getOS :: OS               -- somehow, magically, or see System.Info
+getOS = undefined
+
+separatorF :: OS -> Char
+separatorF os = undefined
+
+-- given an operating system type, return the separator for that OS
+
+-- With that, return the (relative) FilePath for this exercise given various
+-- operating systems. The hierarchy to this resource is:
+
+hierarchy :: [String]
+hierarchy = words "Y2017 M04 D06 Exercise.hs"
+
+exZOS, exDarwin, exWindoze, exOldMac :: String
+exZOS = undefined
+exDarwin = undefined
+exWindoze = undefined
+exOldMac = undefined
+
+{-- BONUS -----------------------------------------------------------------
+
+Define the path to this exercise using YOUR operating system!
+--}
+
+exMyOS :: String
+exMyOS = undefined
+
+-- There's a neat-o trick I learned from the quillo.ink Haskell source today,
+-- but I'll reveal that in the solution.
