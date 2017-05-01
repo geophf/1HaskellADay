6567380a8d7c18cc5abd505d6c8f704d953ddc19
geophf <dauclair@hotmail.com>
2017-05-01 18:40:15 -0400

    Ah! May! When Spring is in the Air, and Haskellers thoughts turn to Love!

---
+++ exercises/HAD/Y2017/M05/D02/Exercise.hs
@@ -0,0 +1,29 @@
+module Y2017.M05.D02.Exercise where
+
+import Prelude hiding (Word)
+
+-- below import available via 1HaskellADay git repository
+
+import Y2017.M04.D12.Exercise
+
+{--
+Another in/not-in puzzler-poem from Mensa Genius Quiz-a-Day Book by
+Dr. Abbie F. Salny
+
+To start the month of May off right, figure out the word concealed in the 
+following poem. ('First,' 'second,' etc refer to the letters of a word.) By
+selecting the right letters you will come up with an appropriate word.
+
+My first is in silly but not in fool.
+My second is in pupil but not in school.
+My third is in read and also write.
+My fourth is in glimpse and also light.
+My fifth is in ten but not in three.
+My last is in glad and also glee.
+My whole is a season of the year
+That's clue enough to solve it here!
+--}
+
+-- as you see from Y2017.M04.D12.Exercise imported above, you've solved this
+-- kind of puzzle before. Define word and rules so that you solve today's
+-- problem
