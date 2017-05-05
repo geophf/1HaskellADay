3961d675676c12662d669a46c18268df01a2da03
geophf <dauclair@hotmail.com>
2017-05-05 09:56:41 -0400

    Happy Cinco de Mayo! ... on a Friday, too! YAY!

---
+++ exercises/HAD/Y2017/M05/D05/Exercise.hs
@@ -0,0 +1,19 @@
+module Y2017.M05.D05.Exercise where
+
+import Data.Time.Calendar
+import Data.Time.Calendar.WeekDate
+
+{--
+Happy Cinco de Mayo, Haskellers!
+
+Check the lock-screen of my iPad (screen shot at this directory) is says
+
+TUESDAY, May 5th.
+
+So, what is the most recent year where Cinco de Mayo was on a Tuesday?
+
+... and how would you go about finding this solution?
+--}
+
+mostRecentYear5MayTuesday :: Integer
+mostRecentYear5MayTuesday = undefined
