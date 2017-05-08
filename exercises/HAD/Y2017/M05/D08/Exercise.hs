844569e302fa7c080947bd8069941ae83edb45d8
geophf <dauclair@hotmail.com>
2017-05-08 11:35:18 -0400

    Well, we're back on the hang-gangs!

---
+++ exercises/HAD/Y2017/M05/D08/Exercise.hs
@@ -0,0 +1,27 @@
+module Y2017.M05.D08.Exercise where
+
+-- below imports available via 1HaskellADay git repository
+
+import Data.Numeral.QBits
+
+{--
+So, we're back on the chain gang.
+
+No: we're back to solving cryptarithmic puzzles via the Mensa Genius Quiz-a-Day
+Book by Dr. Abbie F. Salny, et al.
+
+"In this addition alphametic, you can replace each letter with a number (the
+same number for each letter) to find a correct arithmetic solution.
+
+(Hint: A = 1)
+
+
+      H A N G
+      H A N G
+    + H A N G
+    ---------
+    G A N G S
+--}
+
+hangGangs :: Nums -> Nums -> (Nums, Nums)
+hangGangs hang gangs = undefined
