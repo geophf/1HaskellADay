f01bee270535da17c6d8905f578b0a4c0deee8e7
bazzargh <brian.ewins@gmail.com>
2017-05-17 02:27:20 +0100

    Use OrdinalDate instead of Julian
    See comments in the change

--- exercises/HAD/Y2017/M05/D16/Solution.hs
+++ exercises/HAD/Y2017/M05/D16/Solution.hs
@@ -1,13 +1,13 @@
 module Y2017.M05.D16.Solution where
 
 import Data.Time.Calendar
-import Data.Time.Calendar.Julian
+import Data.Time.Calendar.OrdinalDate
 import Data.Time.Clock
 import Data.Time.LocalTime
 
 {--
 So, yesterday we were able to publish log entries with time-stamps. YAY! AND
-it worked perfectly, because, well, it's 2017 and the Julian date is 123.
+it worked perfectly, because, well, it's 2017 and the day of the year is 134.
 
 And the hour, minute and second were all greater than 10.
 
@@ -42,23 +42,33 @@ Or "Bond. James Bond."
 
 info :: String -> IO String
 info message = getCurrentTime     >>= \dayotimo ->
-   let (yr, jd) = toJulianYearAndDay (utctDay dayotimo)
+   let (yr, doy) = toOrdinalDate (utctDay dayotimo)
        TimeOfDay hr min sec = timeToTimeOfDay (utctDayTime dayotimo)
        theTime = concatMap (`justify` 2) [hr, min, floor sec]
-   in  pure (justify (yr `mod` 100) 2 ++ justify jd 3 ++ theTime
+   in  pure (justify (yr `mod` 100) 2 ++ justify doy 3 ++ theTime
          ++ (' ':message))
 
 {--
 >>> info "ALL YOUR BASE ARE BELONG TO US"
-"17123232155 ALL YOUR BASE ARE BELONG TO US"
+"17134232155 ALL YOUR BASE ARE BELONG TO US"
 >>> info "ALL YOUR BASE ARE BELONG TO US"
-"17123232200 ALL YOUR BASE ARE BELONG TO US"
+"17134232200 ALL YOUR BASE ARE BELONG TO US"
 >>> info "ALL YOUR BASE ARE BELONG TO US"
-"17123232202 ALL YOUR BASE ARE BELONG TO US"
+"17134232202 ALL YOUR BASE ARE BELONG TO US"
 
 TA-DAH!
 
 Oh, and all your base are belong to US! ... ICYMI
 
 MWA-HAHAHA!
+
+(In the original version of this, the Julian day was referred to but what
+was meant was what is loosely called the 'Julian date format', which has
+nothing to do with the Julian Calendar: see https://en.wikipedia.org/wiki/Julian_day#Terminology.
+In Haskell the Data.Time.Calendar.OrdinalDate library gives the pieces required
+for the "Julian Date Format", while Data.Time.Calendar.Julian uses the Julian
+proleptic calendar.
+
+We may have thrown out Data.Time.Calendar.Julian here but can Julian Cope?
+Of course he can. https://www.youtube.com/watch?v=2UJbz-pp6GQ
 --}
