610ea8fae4a87382f770145f7f84f37dd49c730b
geophf <dauclair@hotmail.com>
2017-06-16 13:26:44 -0400

    Updated the Strings returned with quotation marks

--- exercises/HAD/Y2017/M06/D16/Exercise.hs
+++ exercises/HAD/Y2017/M06/D16/Exercise.hs
@@ -32,13 +32,13 @@ dbName = undefined
 
 {-- 
 >>> dbUserName
-geophf
+"geophf"
 >>> dbPassword
-youWish
+"youWish"
 >>> dbmsServer
-pellefant.db.elephantsql.com
+"pellefant.db.elephantsql.com"
 >>> dbName
-1HaskellADay
+"1HaskellADay"
 
 Okay, using the above results, if your connection string is in the format:
 
@@ -52,5 +52,5 @@ connectionString username pass server port db = undefined
 
 {--
 >>> connectionString <$> dbUserName <*> dbPassword <*> dbmsServer <*> pure 5432 <*> dbName
-postgress://geophf:youWish@pellefant.db.elephantsql.com:5432/1HaskellADay
+"postgress://geophf:youWish@pellefant.db.elephantsql.com:5432/1HaskellADay"
 --}
