23eb5b79aa236510967b70eb5360412ac01bde89
geophf <dauclair@hotmail.com>
2017-07-05 09:44:51 -0400

    a * b = c, how hard can that be?

---
+++ exercises/HAD/Y2017/M07/D05/Exercise.hs
@@ -0,0 +1,28 @@
+module Y2017.M07.D05.Exercise where
+
+-- below imports available via 1HaskellADay git repository
+
+import Data.QBit
+import Data.Numeral.QBits
+
+{--
+AHA! I'm BACK!
+I'm BACK in the saddle again!
+
+From the Mensa Genius Quiz-a-Day Book by Dr. Abbie F. Salny, July 3 problem:
+
+The following multiplication example uses all the digits from 0 to 9 once and
+only once (not counting the intermediate steps). Finish the problem. One number
+has been filled in to get you started.
+
+          x x x     (a)
+            x 5     (b)
+      ---------
+      x x x x x     (c)
+
+DO IT TO IT!
+I LIKE TO MOVE IT, MOVE IT!
+--}
+
+multiplicationProblem :: Nums -> Nums -> Nums -> [(Nums, Nums, Nums)]
+multiplicationProblem a b c = undefined
