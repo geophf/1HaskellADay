199fd4fbed3184f2bdf04941aca71f072281ff19
geophf <dauclair@hotmail.com>
2017-07-25 12:14:40 -0400

    Made Atom a Read-instance from replAtom

---
+++ exercises/HAD/Relational/Scheme/Unification/Atoms.hs
@@ -0,0 +1,48 @@
+module Relational.Scheme.Unification.Atoms where
+
+-- below import available via 1HaskellADay git repository
+
+import Control.Logic.Frege ((-|))
+import Relational.Scheme.Types
+
+{--
+So we've created and parsed atomic values, we've run (already evaluated)
+expressions in our evaluator, the next step Reasoned Schemer takes is defining
+the laws of the the relational calculus, and these laws are:
+
+1. Fresh
+2. (≡)
+3. conde
+
+The funny thing is, that you would think we'd define fresh and be cool with 
+that, but no. The Reasoned Schemer demonstrates fresh variables in terms of
+(≡) so, to define fresh, we also, or we first, must define (≡).
+
+So, let's define (≡) with atomic, ground terms.
+--}
+
+unifyAtoms :: Applicative m => Monoid (m ()) => Atom -> Atom -> m ()
+unifyAtoms p q = p == q -| pure ()
+    -- using logic programming to define unification.
+
+{-- that is to say: "If p unifies with q, the statement succeeds."
+
+Given the above definition, what are the results for unifying the below pairs?
+--}
+
+atoms2Unify :: [(Atom, Atom)]
+atoms2Unify = zip [I 5, B True, L Success, S "hi"] [I 5, S "True", B True, S "Hi"]
+
+{--
+>>> zip atoms2Unify ((map (uncurry unifyAtoms) atoms2Unify) :: [[()]])
+[((I 5,I 5),[()]),
+ ((B True,S "True"),[]),
+ ((L #s,B True),[]),
+ ((S "hi",S "Hi"),[])]
+
+We see, above, unification attempted on atoms, regardless of their carried type.
+
+Tomorrow, we will unify ground terms (atoms) with free variables and attempt
+to unify ground terms to bound variables, and so begin to understand logic
+variables.
+--}
