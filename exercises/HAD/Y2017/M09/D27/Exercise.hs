5b0248eb40ca129d4a6352b36d6812ace04eb7fb
geophf <dauclair@hotmail.com>
2017-09-27 13:57:13 -0400

    Added imports to load articles to examine for name-parsing

--- exercises/HAD/Y2017/M09/D27/Exercise.hs
+++ exercises/HAD/Y2017/M09/D27/Exercise.hs
@@ -1,12 +1,22 @@
 module Y2017.M09.D27.Exercise where
 
+import qualified Data.Map as Map
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+-- below import available via 1HaskellADay git repository
+
+import Y2017.M09.D22.Exercise (dir, arts)      
+import Y2017.M09.D25.Solution (parseKV)
+import Y2017.M09.D26.Exercise (extractArticles)
+
 {--
 What's in a name?
 
-So, (day before) yesterday, you were able to break down a block of text into a 
+So, (day before) yesterday, you were able to break down a block of text into a
 set of attributed values that make up an article. One of those attributed values
 was author. Another possible attributed value (in the metadata) is "People":
 
+>>> (art3:articles) <- extractArticles <$> BL.readFile (dir ++ arts)
 >>> author art3
 "Nagourney, Adam"
 >>> Map.lookup "People" (metadata art3)
@@ -15,7 +25,7 @@ Just "Cuomo, Mario M"
 But here's the thing. A person's name has structure in the data store:
 --}
 
-data Person = Name { idpers :: Int, source, family, given, rest :: String }
+data Person = Name { source, family, given, rest :: String }
    deriving (Eq, Ord, Show)
 
 -- parse the author and "People" metadata to Person values
@@ -31,6 +41,9 @@ multis = "People: Putin, Vladimir Batiashvili, Lisa Gergiev, Valery"
 -- Look at the structure of these names and as compared to Cuomo's name or
 -- Nagourney's name. Hm. Parse out the above and have the parser work for
 -- Cuomo, multis and Nagourney.
+
+-- hint: you may wish to use parseKV from the Y2017.M09.D25 import to help
+-- separate the "People:"-key from the names in the multis function.
 
 -- Also, this one, because why? Because real data is unpredictable:
 
