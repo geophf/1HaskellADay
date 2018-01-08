d7e5abd94fe4979422a4a6e9888f408b32073f78
geophf <dauclair@hotmail.com>
2018-01-08 14:53:55 -0500

    Describes documents that have a body of content

---
+++ exercises/HAD/Data/HTML.hs
@@ -0,0 +1,26 @@
+module Data.HTML where
+
+-- Describes a thing that has a body of content.  No. Really.
+
+import Data.Maybe (mapMaybe)
+import Text.HTML.TagSoup -- which you can get from cabal
+
+class HTML a where
+   body :: a -> [String]
+
+-- 1. the unparsed string as a block of HTML
+
+htmlBlock :: HTML a => a -> String
+htmlBlock = unlines . body
+
+-- 2. the parsed text as a set of lines of tag-free text
+
+plainText :: HTML a => a -> [String]
+plainText = map demark . body
+
+demark :: String -> String
+demark = unwords . mapMaybe text . parseTags
+
+text :: Tag String -> Maybe String
+text (TagText s) = Just s
+text _           = Nothing
