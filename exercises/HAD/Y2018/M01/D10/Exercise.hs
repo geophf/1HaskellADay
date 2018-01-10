b31913eba2d6f931ab61c48409b622aca87ae881
geophf <dauclair@hotmail.com>
2018-01-10 15:31:37 -0500

    Added reminder to include new logging functionality into the new etl-process

--- exercises/HAD/Y2018/M01/D10/Exercise.hs
+++ exercises/HAD/Y2018/M01/D10/Exercise.hs
@@ -26,6 +26,7 @@ import Database.PostgreSQL.Simple.ToRow
 
 import Y2017.M12.D20.Exercise -- for Packet
 import Y2018.M01.D04.Exercise hiding (etl)  -- hint for bonus
+import Y2018.M01.D09.Exercise -- for logging
 
 instance ToRow Packet where
    toRow pack = undefined
@@ -38,5 +39,6 @@ information.
 {-- BONUS -----------------------------------------------------------------
 
 Rewrite the etl-process to store the packet information (that is: don't discard
-packet information anymore) along with the articles.
+packet information anymore) along with the articles. Remember to include logging
+functionality as well!
 --}
