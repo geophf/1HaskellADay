e00f482f98adba5d59537a378cbf28efd962febb
geophf <dauclair@hotmail.com>
2018-01-11 06:59:44 -0500

    Added an row-entry type for the logs and hardened the severity value

--- exercises/HAD/Y2018/M01/D09/Exercise.hs
+++ exercises/HAD/Y2018/M01/D09/Exercise.hs
@@ -17,6 +17,8 @@ today, let's record what we've logged into the database with a new log-table.
 
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.SqlQQ
+import Database.PostgreSQL.Simple.ToField
+import Database.PostgreSQL.Simple.ToRow
 
 -- below imports available via 1HaskellADay git repository
 
@@ -24,10 +26,24 @@ import Data.Logger
 
 import Y2018.M01.D02.Exercise hiding (etl)
 
-insertLogEntryStmt :: Query
-insertLogEntryStmt = [sql|INSERT INTO log (entry) VALUES (?)|]
+data Severity = TRACE | DEBUG | INFO | WARN | ERROR | FATAL
+   deriving (Eq, Ord, Show)
 
-insertLogEntries :: Connection -> [String] -> IO ()
+instance ToField Severity where
+   toField s = undefined
+
+data LogEntry = Entry { sev :: Severity,app,mod,msg :: String }
+   deriving (Eq, Show)
+
+instance ToRow LogEntry where
+   toRow entr = undefined
+
+insertLogEntryStmt :: Query
+insertLogEntryStmt =
+   [sql|INSERT INTO log (severity,app,module,message)
+        VALUES ((SELECT id FROM severity_lk WHERE level=?),?,?,?)|]
+
+insertLogEntries :: Connection -> [LogEntry] -> IO ()
 insertLogEntries conn entries = undefined
 
 -- modify the ETL process from Y2018.M01.D02.Exercise to spill the log entries
