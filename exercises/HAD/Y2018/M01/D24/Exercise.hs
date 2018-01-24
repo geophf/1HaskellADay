96ca269624ce0400987c51ae82354cd121b5d45c
geophf <dauclair@hotmail.com>
2018-01-24 03:31:13 -0500

    Added sample string to test against and test run results

--- exercises/HAD/Y2018/M01/D24/Exercise.hs
+++ exercises/HAD/Y2018/M01/D24/Exercise.hs
@@ -13,10 +13,16 @@ Example:
 X = [[a,a,a,a],[b],[c,c],[a,a],[d],[e,e,e,e]]
 --}
 
+sample :: String
+sample = "aaaabccaadeeee"
+
 pack :: Eq a => [a] -> [[a]]
 pack list = undefined
 
 {--
+>>> pack sample
+["aaaa","b","cc","aa","d","eeee"]
+
 P10 (*) Run-length encoding of a list.
 Use the result of problem P09 to implement the so-called run-length encoding 
 data compression method. Consecutive duplicates of elements are encoded as 
@@ -31,6 +37,9 @@ encode :: Eq a => [a] -> [(Int, a)]
 encode list = undefined
 
 {--
+>>> encode sample
+[(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+
 P12 (**) Decode a run-length encoded list.
 Given a run-length code list generated as specified in problem P10. Construct 
 its uncompressed version.
@@ -38,6 +47,14 @@ its uncompressed version.
 
 decode :: [(Int, a)] -> [a]
 decode pairs = undefined
+
+{--
+>>> decode it
+"aaaabccaadeeee"
+
+>>> it == sample
+True
+--}
 
 -- I'm also reading Tarski's Introduction to Logic, 1933, so some Haskell
 -- problems may come from that.
