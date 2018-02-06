9526081363d24fbcc6ccf2e3d7db5637f96ae3ae
geophf <dauclair@hotmail.com>
2018-02-05 22:00:44 -0500

    Logging: now timestamped!

--- exercises/HAD/Y2018/M02/D05/Solution.hs
+++ exercises/HAD/Y2018/M02/D05/Solution.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE QuasiQuotes #-}
 
-module Y2018.M02.D05.Exercise where
+module Y2018.M02.D05.Solution where
 
 {--
 So, our log-type assumes that the time of insertion is the record of when the
@@ -13,6 +13,7 @@ Let's enhance the LogEntry type with timestamp. Then, when we insert that
 entry into the database, let's add that timestamp.
 --}
 
+import Data.Functor (void)
 import Control.Monad.Writer
 
 import Data.Time
@@ -26,32 +27,33 @@ import Database.PostgreSQL.Simple.ToRow
 
 -- below imports available via 1HaskellADay git repository
 
-import Control.DList (DList)
+import Control.DList (DList, dl')
 
 import Data.LookupTable (LookupTable)
-import Data.Logger (LogEntry)
+import Data.Logger (LogEntry, LogEntry'(LE'))
 
-import Store.SQL.Util.Logging
+-- import Store.SQL.Util.Logging
 
 data Stamped a = Stamped { stamped :: a, time :: LocalTime }
    deriving Show
 
 instance ToRow a => ToRow (Stamped a) where
-   toRow se = undefined
+   toRow se = toField (time se):toRow (stamped se)
 
 -- hint: see below. I put the time-field at the head of the insert columns
 
 stampIt :: a -> IO (Stamped a)
-stampIt entry = undefined
+stampIt e =
+   getCurrentTime >>= \t -> Stamped e <$> (`utcToLocalTime` t) <$> getTimeZone t
 
 -- takes a log entry and puts the current timestamp on it
 
-type StampedLogger = WriterT (DList (Stamped LogEntry)) IO
+type StampedWriter a = WriterT (DList (Stamped a)) IO
 
 -- sayIO may be an useful function to define as well:
 
-sayIO :: LogEntry -> StampedLogger ()
-sayIO entry = undefined
+sayIO :: a -> StampedWriter a ()
+sayIO entry = lift (dl' <$> stampIt entry) >>= tell
 
 -- ... as it 'automatically' timestamps a logentry for you as it puts it in
 -- the log.
@@ -60,12 +62,20 @@ insertStampedEntryStmt :: Query
 insertStampedEntryStmt =
    [sql|INSERT INTO log (time,severity,app,module,message) VALUES (?,?,?,?,?)|]
 
+{-- moving to Store.SQL.Util.Logging:
 insertStampedEntries :: Connection -> LookupTable -> [Stamped LogEntry] -> IO ()
-insertStampedEntries conn lktab entries = undefined
+insertStampedEntries conn lktab =
 
 -- hint: you may wish to introduce an intermediary type to handle insertion of
 -- severity-values, as managed for LogEntry values in Store.SQL.Util.Logging
 
--- Once defined, the concept of Stamped will be moved to Store.SQL.Util.Stamped
+-- okay, bad hint. We need to unwrap the log entries and make them LogEntry'
+-- values
 
--- The new stamped log entry type will be rolled into Store.SQL.Util.Logging
+   void . executeMany conn insertStampedEntryStmt
+        . map (\(Stamped a t) -> Stamped (LE' a lktab) t)
+--}
+
+-- The concept of Stamped will be moved to Data.Stamped and 
+-- Store.SQL.Util.Stamping. The new stamped log entry type will be rolled into 
+-- Store.SQL.Util.Logging
