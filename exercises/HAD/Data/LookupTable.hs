d053b1ec569dbf3843693095ef5babc8c2a4c212
geophf <dauclair@hotmail.com>
2018-02-11 06:24:16 -0500

    Added the dual of the LookupTable: LookDown and the generator function.

--- exercises/HAD/Data/LookupTable.hs
+++ exercises/HAD/Data/LookupTable.hs
@@ -1,7 +1,16 @@
 module Data.LookupTable where
 
--- Gives the index for a lookup value
+-- Gives the index for a lookup value (useful for storing data from a lookup)
 
 import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Tuple (swap)
 
 type LookupTable = Map String Integer
+
+-- Now we need to flip the table for when we're doing fetches
+
+type LookDown = Map Integer String
+
+lookdown :: LookupTable -> LookDown
+lookdown = Map.fromList . map swap . Map.toList
