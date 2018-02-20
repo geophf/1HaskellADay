a74353e1968bbfbfb43a312aa647f25aa3c08e35
geophf <dauclair@hotmail.com>
2018-02-19 21:57:17 -0500

    More fun with elements in a list! Whee!

---
+++ exercises/HAD/Y2018/M02/D20/Exercise.hs
@@ -0,0 +1,24 @@
+module Y2018.M02.D20.Exercise where
+
+{--
+From our favorite Prolog problem archive, P99:
+
+http://www.ic.unicamp.br/~meidanis/courses/mc336/2009s2/prolog/problemas/
+
+P14 (*) Duplicate the elements of a list.
+Example:
+?- dupli([a,b,c,c,d],X).
+X = [a,a,b,b,c,c,c,c,d,d]
+--}
+
+dupli :: [a] -> [a]
+dupli list = undefined
+
+-- What is the sum of a list of numbers?
+-- What is the sum of a list of duplicated numbers?
+
+list0nums :: [Int]
+list0nums = [50,88,47,46,55,35,4,66,27,65]
+
+-- numbers generated at random.org
+
