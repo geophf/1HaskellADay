30656ccfb775459e25f5c0b9f14d8f4a3041df3e
geophf <dauclair@hotmail.com>
2018-02-22 12:36:37 -0500

    Counting in Haskell IN YOUR NATIVE LANGUAGE!

---
+++ exercises/HAD/Y2018/M02/D22/Exercise.hs
@@ -0,0 +1,28 @@
+module Y2018.M02.D22.Exercise where
+
+{--
+Another one from P99, via the web on
+
+http://www.ic.unicamp.br/~meidanis/courses/mc336/2009s2/prolog/problemas/
+
+P95 (**) English number words
+On financial documents, like cheques, numbers must sometimes be written in full 
+words. Example: 175 must be written as one-seven-five. Write a predicate 
+full_words/1 to print (non-negative) integer numbers in full words.
+--}
+
+fullWords :: Int -> String
+fullWords n = undefined
+
+{-- BONUS -----------------------------------------------------------------
+
+Now, output the number in your native language... Finnish will be fun!
+
+Note, the example above is NOT in English, so, if your native language is
+English, output "one hundred seventy-five" not this "one-seven-five" weirdness.
+--}
+
+realWords :: Int -> String
+realWords n = undefined
+
+-- so your lexicon will be ... a 'bit' richer, eh?
