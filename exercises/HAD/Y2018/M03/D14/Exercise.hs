1dc2b9358c3320ea724f8abca9a0f043c55502f1
geophf <dauclair@hotmail.com>
2018-03-14 11:08:36 -0400

    Thinking about sumaSumasCuadradosDivisores from the Haskell community

---
+++ exercises/HAD/Y2018/M03/D14/Exercise.hs
@@ -0,0 +1,16 @@
+module Y2018.M03.D14.Exercise where
+
+{--
+Today's Haskell exercise we pull from the community:
+
+http://www.glc.us.es/~jalonso/exercitium/suma-de-las-sumas-de-los-cuadrados-de-los-divisores/
+
+The sums of the sums of the squares of the divisors of n, or:
+--}
+
+sumaSumasCuadradosDivisores :: Integer -> Integer
+sumaSumasCuadradosDivisores n = undefined
+
+-- how would you go about doing this? For small n, okay, give it a go!
+-- for rather large n, there's some interesting discussion (in Spanish) at
+-- the URL above.
