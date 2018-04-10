3a10106128e6479ef6d184edd6d5a9b1f1744c90
geophf <dauclair@hotmail.com>
2018-04-10 18:44:42 -0400

    Hey, Mister! You got the time?

---
+++ exercises/HAD/Y2018/M04/D10/Solution.hs
@@ -0,0 +1,16 @@
+module Y2018.M04.D10.Solution where
+
+-- What time is it right now? That's today's Haskell exercise
+
+import Data.Time.Clock
+import Data.Time.LocalTime
+
+now :: IO LocalTime
+now = utcToLocalTime <$> getCurrentTimeZone <*> getCurrentTime
+
+{--
+>>> now
+2018-04-09 12:32:05.796303
+
+This module is being moved to the 1HaskellADay Data.Time.Now module
+--}
