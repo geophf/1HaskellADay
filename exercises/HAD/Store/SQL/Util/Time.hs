536c7c5857407ea253c85c3017e9ae480b18ff24
geophf <dauclair@hotmail.com>
2018-05-12 22:25:50 -0400

    Added UTC <-> Day conversions

--- exercises/HAD/Store/SQL/Util/Time.hs
+++ exercises/HAD/Store/SQL/Util/Time.hs
@@ -3,8 +3,17 @@ module Store.SQL.Util.Time where
 -- converts SQL dates to Haskell days
 
 import Data.Time
+import Data.Time.Clock
+
 import Database.PostgreSQL.Simple.Time
 
 d2d :: LocalTimestamp -> Maybe Day
 d2d (Finite d) = Just (localDay d)
 d2d _          = Nothing
+
+utc2d :: UTCTimestamp -> Maybe Day
+utc2d (Finite d) = Just (utctDay d)
+utc2d _          = Nothing
+
+d2utc :: Day -> UTCTimestamp
+d2utc = Finite . flip UTCTime (secondsToDiffTime 0)
