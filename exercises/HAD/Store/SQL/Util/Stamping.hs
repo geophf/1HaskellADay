50320a5df7a760038a54d705456d6764505c19d1
geophf <dauclair@hotmail.com>
2018-05-14 21:33:48 -0400

    Added fromRow functionality for stamping to retrieve the active audit log entry

--- exercises/HAD/Store/SQL/Util/Stamping.hs
+++ exercises/HAD/Store/SQL/Util/Stamping.hs
@@ -1,11 +1,22 @@
 module Store.SQL.Util.Stamping where
 
+import Data.Time (LocalTime)
+
+import Database.PostgreSQL.Simple.FromField
+import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.Time
 import Database.PostgreSQL.Simple.ToField
 import Database.PostgreSQL.Simple.ToRow
 
 -- below import available via 1HaskellADay git repository
 
-import Data.Time.Stamped (Stamped, time, stamped)
+import Data.Time.Stamped (Stamped(Stamped), time, stamped)
 
 instance ToRow a => ToRow (Stamped a) where
    toRow se = toField (time se):toRow (stamped se)
+
+instance FromRow a => FromRow (Stamped a) where
+   fromRow = Stamped <$> fromRow <*> (parseTimestamp <$> field) 
+
+parseTimestamp :: LocalTimestamp -> LocalTime
+parseTimestamp (Finite ts) = ts
