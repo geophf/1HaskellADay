8ef7eabe9b43425aa3e7eb1c9eef76df9c132817
geophf <dauclair@hotmail.com>
2018-05-24 12:09:03 -0400

    Adding articles to be cleaned up, post-ETL, to a dirty table.

--- exercises/HAD/Y2018/M05/D24/Exercise.hs
+++ exercises/HAD/Y2018/M05/D24/Exercise.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 
-module Y2018.M05.D21.Exercise where
+module Y2018.M05.D24.Exercise where
 
 {--
 One more bit before we insert rows.
@@ -9,19 +9,6 @@ We need to inform the wider audience which articles we've inserted and
 updated so they can do stuff. The 'stuff,' in particular, that they are going
 to do is the python-y cleaning of these new and updated articles, and they are
 doing it in python because that's how they roll.
-
-Incidentally, I was able to download an entire article archive from a REST
-endpoint and then upload that article archive with data and metadata to a
-PostgreSQL RDS in a couple of minutes.
-
-The Python cleaning interacted solely with the RDS and took 1 hour 10 minutes.
-
-But, there you have it: industrial-strength Python (3.6) is in the mainstream
-and Haskell is not because Python is ...
-
-... 50-100x slower.
-
-Anyway, now that I'm done trollin' on the Great Language Debate Flame War ...
 
 For all NEW and UPDATED articles in your triaged set, insert the id into
 a new data table: unclean. Later a Python batch job will pick up these ids and
@@ -41,6 +28,8 @@ import Store.SQL.Util.Indexed
 import Y2018.M05.D04.Exercise (downloader)
 import Y2018.M05.D07.Exercise (fetchArticleMetaData)
 import Y2018.M05.D08.Exercise -- for Triage and WPJATI
+
+-- this is a bit of misdirection: Why?
 
 markDirtyArticles :: Connection -> Map Triage [WPJATI] -> IO ()
 markDirtyArticles triaged = undefined
