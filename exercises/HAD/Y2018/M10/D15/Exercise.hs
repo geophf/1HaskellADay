a6f285f446a2a997988f7a2070fe07e15c8c68f1
geophf <dauclair@hotmail.com>
2018-10-14 18:45:08 -0400

    A little asset shell-game

---
+++ exercises/HAD/Y2018/M10/D15/Exercise.hs
@@ -0,0 +1,25 @@
+module Y2018.M10.D15.Exercise where
+
+{--
+You have a set of assets on your iPod, a piece of hardware older than your
+daughters, let's say, hypothetically, and you have a sets of assets on your
+thumbdrives. You want to move the assets off your iPod before the needle wrecks
+your hard drive. I'm not kidding.
+
+With the assets defined on your iPod and then the assets defined on each of
+your thumbdrive, what assests do you need to move off your iPod without
+moving assets that you already have on your thumbdrives?
+--}
+
+type Assets = [String]
+
+iPod, thumb1, thumb2, exDir :: FilePath
+exDir = "Y2018/M10/D15/"
+iPod = "iPod.sha"
+thumb1 = "thumb1.sha"
+thumb2 = "thumb2.sha"
+
+-- Given the files with the assets, what assets need to be moved off the iPod?
+
+assetsToMove :: FilePath -> FilePath -> FilePath -> IO Assets
+assetsToMove ipod th1 th2 = undefined
