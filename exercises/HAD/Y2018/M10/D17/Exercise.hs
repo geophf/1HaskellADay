37fab38d2c8fb0b35f43ff6a3baa66e84e4c79cb
geophf <dauclair@hotmail.com>
2018-10-17 04:28:05 -0400

    Using Haskell to automate writing a set of SQL INSERT statements

---
+++ exercises/HAD/Y2018/M10/D17/Exercise.hs
@@ -0,0 +1,33 @@
+module Y2018.M10.D17.Exercise where
+
+{--
+Today. Write a function that takes 2 dates then writes a set of SQL INSERT
+statements that enumerates every day between those days into the packet
+table.
+
+The packet table structure is
+
+packet table
+------------
+id,serial
+download_dt,timestamp
+from_date,date
+to_date,date
+raw_xml,text
+downloaded,text
+--}
+
+import Data.Time
+
+type Statement = String
+
+datesBetween :: Day -> Day -> [Statement]
+datesBetween from to = undefined
+
+{--
+>>> datesBetween (read "2018-09-10") (read "2018-10-10")
+["INSERT INTO packet (from_date,to_date) VALUES ('2018-09-10','2018-09-11');",
+ "INSERT INTO packet (from_date,to_date) VALUES ('2018-09-11','2018-09-12');",
+ "INSERT INTO packet (from_date,to_date) VALUES ('2018-09-12','2018-09-13');",
+ ...]
+--}
