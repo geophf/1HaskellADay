e3365c64b368e5dea37e82466c17fc8c1922d738
geophf <dauclair@hotmail.com>
2018-11-21 17:25:09 -0500

    Throw enough 1/4th together and you get 1/3. Trust me on this one.

---
+++ exercises/HAD/Y2018/M11/D22/Exercise.hs
@@ -0,0 +1,28 @@
+module Y2018.M11.D22.Exercise where
+
+{--
+Happy Thanksgiving from the U.S.A.!
+
+From @fermatslibrary:
+
+A visual proof that
+1/4 + 1/4² + 1/4³ + ... = 1/3
+
+⚪⚪⚪⚪⚪⚪⚪⚪
+⚪⚫⚪⚪⚪⚪⚪⚪
+⚪⚪⚫⚫⚪⚪⚪⚪
+⚪⚪⚫⚫⚪⚪⚪⚪
+⚪⚪⚪⚪⚫⚫⚫⚫
+⚪⚪⚪⚪⚫⚫⚫⚫
+⚪⚪⚪⚪⚫⚫⚫⚫
+⚪⚪⚪⚪⚫⚫⚫⚫
+
+Prove this.
+
+How many terms of fourths to be within 0.1 of 1/3? 0.01? 0.001?
+--}
+
+fourthThird :: Int -> Double
+fourthThird nterms = undefined
+
+-- fourthThird gives the difference of the sum of fourths vs 1/3.
