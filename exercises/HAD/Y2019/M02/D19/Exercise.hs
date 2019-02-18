4fef786c9f88c1022b2b785a3dd7b11b17ef85c5
geophf <dauclair@hotmail.com>
2019-02-18 18:46:08 -0500

    Now that we have a word finder, let's pretty-print the results.

---
+++ exercises/HAD/Y2019/M02/D19/Exercise.hs
@@ -0,0 +1,19 @@
+module Y2019.M02.D19.Exercise where
+
+{--
+So, yesterday we found words, but the reportage of these words left something
+to be desired for me.
+
+Today's Haskell problem is to take the results from yesterday and pretty-print
+them
+--}
+
+import Data.Map (Map)
+import Data.Traversable
+
+-- below imports available via 1HaskellADay git repository
+
+import Y2019.M02.D18.Exercise
+
+pprintML :: WordsByLength -> IO ()
+pprintML wrds = undefined
