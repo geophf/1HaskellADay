8fa79c4be7318bc97b1cc22c938541212d9e2c5e
geophf <dauclair@hotmail.com>
2020-07-16 08:47:32 -0400

    Added expected results

--- exercises/HAD/Y2020/M07/D16/Exercise.hs
+++ exercises/HAD/Y2020/M07/D16/Exercise.hs
@@ -21,6 +21,9 @@ type WorldEnergy = Int
 type Recruits = Int
 type KnowledgeBase = Map Level (WorldEnergy, Recruits)
 
+daterzFile :: FilePath
+daterzFile = "Y2020/M07/D16/daterz.csv"
+
 dataset :: FilePath -> IO KnowledgeBase
 dataset file = undefined
 
@@ -32,4 +35,13 @@ levelsRanked kb = undefined
 {--
 levelsRanked returns the level with the highest R / WE as the first element.
 I suppose you could use a heap, instead, to automate ranking, if you wish.
+
+These were my results:
+
+>>> dataset daterzFile >>= return . levelsRanked 
+[("s1:8-4",233.33%),("s1:7-2",200.00%),("s1:7-4",200.00%),("s2:5-2N",183.33%),
+ ("s2:21-10N",175.00%),("s3:7-10N",166.66%),("s3:7-4N",166.66%),
+ ("s2:27=1N",133.33%),("s3:11-2N",128.57%)]
+
+... what do you come up with?
 --}
