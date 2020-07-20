ca53348a0b792086792134e00da22215b9d4fb96
geophf <dauclair@hotmail.com>
2020-07-20 15:55:15 -0400

    Reading a file that contains a JSON message and parsing it.

---
+++ exercises/HAD/Y2020/M07/D20/Exercise.hs
@@ -0,0 +1,26 @@
+module Y2020.M07.D20.Exercise where
+
+{--
+JSON processing!
+
+So, this message/notification:
+--}
+
+import Data.Aeson
+
+deleteMsg :: FilePath
+deleteMsg = "Y2020/M07/D20/delete_notif.json"
+
+-- is it valid JSON? Can you parse it
+
+readNotification :: FromJSON a => FilePath -> IO a
+readNotification msg = undefined
+
+-- given that we can read this thingie (that's a technical term), what
+-- is the text of message body? That is to day: what airspace are we deleting?
+
+airspace :: a -> String
+airspace msg = undefined
+
+-- Of course, you'll have to structure the 'a' data-type in order to retrieve
+-- the message.
