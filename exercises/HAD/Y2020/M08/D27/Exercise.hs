c57b0b1702a72917efe3a18a31d78bdacecaf123
geophf <dauclair@hotmail.com>
2020-08-27 19:15:04 -0400

    Updated the exercise with sample outputs and a new function to define.

--- exercises/HAD/Y2020/M08/D27/Exercise.hs
+++ exercises/HAD/Y2020/M08/D27/Exercise.hs
@@ -38,6 +38,22 @@ charCount :: Text -> CharFreq
 charCount book = undefined
 
 {-- 
+>>> let the_index = gutenbergIndex (workingDir ++ gutenbergTop100Index)
+>>> let xmas_info = head . Map.toList <$> the_index
+>>> xmas_info
+("A Christmas Carol in Prose; Being a Ghost Story of Christmas by Charles Dickens (403)",
+ "http://www.gutenberg.org//ebooks/46")
+>>> let xmas_carol = importBook =<< xmas_info
+>>> length <$> xmas_carol
+182057
+
+>>> Map.map getSum . charCount <$> xmas_carol
+{('\n',4238),('\r',4238),(' ',28438),('!',411),('"',1388),('#',1),('$',2),
+ ('%',1),('\'',456),('(',37),(')',37),('*',28),(',',2906),('-',476),('.',1640),
+ ('/',24),('0',20),('1',60),('2',10),('3',13),('4',17),('5',12),('6',11),
+ ('7',6),('8',14),('9',9),(':',98),(';',370),('?',159),('@',2),('A',270),
+ ('B',199),('C',235),('D',108),('E',187),('F',135),('G',254),('H',261),...}
+
 Now find the 'weird words' in the book's text. What are 'weird words'?
 'weird words' are words that you didn't expect would be words but are
 words, and they are weirding you out. Words like "\138\016\054" ... and
@@ -46,5 +62,24 @@ stuff.
 
 weirdWords :: Text -> Set String
 weirdWords book = undefined
+
+{--
+>>> let weirds = weirdWords <$> xmas_carol 
+>>> weirds
+{"\"'And","\"A","\"Ah!\"","\"All","\"Always","\"Am","\"An","\"And",...}
+
+>>> length <$> weirds
+3759
+
+So: what are the weird characters in the weird words?
+--}
+
+weirdChars :: Set String -> Set Char
+weirdChars weirds = undefined
+
+{--
+>>> weirdChars <$> weirds
+{"!\"#$%'()*,-./0123456789:;?@[]\182\187\191"}
+--}
 
 -- with this analysis, we'll be able to write a word-vectorizer, ... tomorrow.
