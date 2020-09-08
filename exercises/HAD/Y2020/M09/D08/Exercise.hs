d83a39004a689c8480c6ca4db1505e1cd47ba337
geophf <dauclair@hotmail.com>
2020-09-08 15:06:47 -0400

    Provided expected results from the data analyses.

--- exercises/HAD/Y2020/M09/D08/Exercise.hs
+++ exercises/HAD/Y2020/M09/D08/Exercise.hs
@@ -29,24 +29,81 @@ Today's Haskell Exercise: data analyses.
 import Data.Map (Map)
 import Data.Set (Set)
 
+import Y2020.M08.D25.Solution (gutenbergIndex, workingDir, gutenbergTop100Index)
+import Y2020.M08.D26.Solution (importLibrary)
+import Y2020.M08.D31.Solution (stopwords, loadStopwords)
+
 type WordOccurrences = Map String Int
- 
+
+-- A little helper-function to load the ontology
+
+ont :: IO Ontology
+ont = let nupes = loadStopwords stopwords
+          idx = gutenbergIndex (workingDir ++ gutenbergTop100Index)
+          lib = idx >>= importLibrary
+      in  bookVec <$> nupes <*> lib
+
+{--
+>>> ont
+... zillions of entries later ...
+>>> let mont = it
+
+... `mont` stands for `my ont(ology)`
+
+... now we need to get the WordOccurrences
+
+>>> let wc = ontology mont
+--}
+
 -- 0. How many words are in all the books?
 
 allWordsCount :: WordOccurrences -> Int
 allWordsCount wordcounts = undefined
 
--- WordOccurrences come from Y2020.M09.D01.Exercise.ontology
+{--
+>>> allWordsCount wc
+251429
+
+That took a little while.
+--}
 
 -- 1. How many words, and what are the words that occur in all 100 books?
 
 inAllBooks :: WordOccurrences -> Set String
 inAllBooks wordcounts = undefined
 
+{--
+>>> let iab = inAllBooks wc
+>>> iab
+{"body","ebook","ebooks","free","gutenberg","library",
+ "page","project","search","start"}
+
+>>> length iab
+10
+--}
+
 -- 2. How many words, and what are the words, that occur only once?
 
 onlyOneOccurringWords :: WordOccurrences -> Set String
 onlyOneOccurringWords wordcounts = undefined
+
+{--
+Hm:
+
+>>> let ooow = onlyOneOccurringWords wc
+>>> ooow
+
+lots of messy words, but then we also have words of this form:
+
+...  "liveries\226\128\157" ...
+
+It looks like we may need to clean up our clean-up algorithm, because:
+
+>>> length ooow
+195035
+
+Is a lot of words. Some of those words may be useful in clustering (later).
+--}
 
 -- 3. Which words occur only once in a book (any book) ... even if those words
 -- occur in multiple books, if they occur just once in any book, what are these
@@ -55,16 +112,35 @@ onlyOneOccurringWords wordcounts = undefined
 oneWordInBook :: Ontology -> Set String
 oneWordInBook bookswords = undefined
 
+{--
+>>> let ooib = oneWordInBook mont
+>>> ooib
+
+... lots of words, again ...
+
+>>> length ooib
+205354
+
+Good Heavens!
+--}
+
 -- 4. Okay. Remove all the one-word and all-books-words from out ontology.
 
 removeInfreqs :: Set String -> Ontology -> Ontology
 removeInfreqs infrequentWords ont = undefined
 
--- Recompute the WordOccurrences from that trimmed Ontology. What is
--- allWordsCount now?
+{--
+>>> let newOnt = removeInfreqs (Set.unions [iab, ooow, ooib]) mont
+>>> length newOnt
+100
 
--- BONUS -------------------------------------------------------
+... as expected, as there are still 100 books being codified, but ...
 
--- 5. Repeat the above for words in 99 books and only 2 books. What are the
--- results from that trimming?
+>>> let newWc = ontology newOnt 
+>>> allWordsCount newWc
+2333
 
+Wow! Huge difference! Good or bad?
+
+We will find that out on another, fine Haskell-problem-solving day!
+--}
