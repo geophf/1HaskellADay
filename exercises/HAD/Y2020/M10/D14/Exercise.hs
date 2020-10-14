90730cc0861ce5c3515253c8b03150ff7f58fb36
geophf <dauclair@hotmail.com>
2020-10-14 14:46:34 -0400

    Countries by Continent from a ... 'markdown' file? That is not markdown.

---
+++ exercises/HAD/Y2020/M10/D14/Exercise.hs
@@ -0,0 +1,59 @@
+module Y2020.M10.D14.Exercise where
+
+{--
+I really hate wikidata sometimes.
+
+The query:
+
+# Continents/Countries
+SELECT ?continent ?continentLabel ?country ?countryLabel # ?region ?regionLabel # ?particularRegion ?particularRegionLabel 
+WHERE 
+{
+  ?continent wdt:P31 wd:Q5107.
+  ?country wdt:P31 wd:Q6256.
+  # ?region wdt:P31 wd:Q82794.
+  # ?region wdt:p642 ?continent.
+  # ?particularRegion wdt:p361 ?region.
+  ?country wdt:P361 ?continent.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+
+says there are only two countries in Europe: Norway and Italy.
+
+It also says there are only seventeen countries that are in continents in the
+entire World. If you're not one of these seventeen countries, then, as Cee Lo
+Green sings: "[Forget] you."
+
+You see that some RDF triples are commented out. Removing the comment marker
+reduces the result set to 0 countries and continents, even though manual
+search shows otherwise.
+
+[Forget] you, wikidata.
+
+But, there is a wiki page that lists countries by continent. So I did a hand-
+scrape of that page.
+
+Today's Haskell problem, let's convert that scrape to Haskell data.
+--}
+
+import Data.Map (Map)
+
+workingDir :: FilePath
+workingDir = "Y2020/M10/D14/"
+
+cbc :: FilePath
+cbc = "countries.txt"
+
+type Continent = String
+type Country = String
+
+countriesByContinent :: FilePath -> IO (Map Continent [Country])
+countriesByContinent countriesFile = undefined
+
+{--
+A note about formatting:
+
+deal with it.
+
+That is all.
+--}
