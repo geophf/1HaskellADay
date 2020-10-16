ad15350e059564151d479942542c7fc58815bb56
geophf <dauclair@hotmail.com>
2020-10-16 11:00:47 -0400

    Map of a -> b or b -> a? ... If you're asking that question, why not Graph?

---
+++ exercises/HAD/Y2020/M10/D16/Exercise.hs
@@ -0,0 +1,42 @@
+module Y2020.M10.D16.Exercise where
+
+
+{--
+`sequence` is really cool: a neat trick.
+
+The problem is that we have to use it in the first place. The question becomes
+if the data structure you are using doesn't give you what you need, then you
+need to move to a better data model. Data.Map isn't bidirecional, and that's
+fine in many cases, but isn't in this case where we needed to key from the
+country to the continent, and not the waythe original structure was built.
+
+So. What data structure?
+
+Hello, Graph.
+--}
+
+import Graph.Query
+
+-- or, if you prefer:
+
+import Data.Graph
+
+import Y2020.M10.D14.Exercise
+
+{--
+Take the countries and continents from before and structure these data in a
+graph.
+
+In which continent is Andorra?
+What are the countries in ... 'Oceania'?
+What are all the continents? What is each continent's countries?
+--}
+
+continentOf :: Graph -> Country -> Continent
+continentOf = undefined
+
+countriesOf :: Graph -> Continent -> [Country]
+countriesOf = undefined
+
+continentsCountries :: Graph -> [(Continent, [Country])]
+continentsCountries = undefined
