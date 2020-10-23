76aad4a20b5e52b61f0d8e4e1d87e23d4be6888d
geophf <dauclair@hotmail.com>
2020-10-23 10:31:50 -0400

    Unicode? What, even, is that? ... sez Haskell, smh

--- exercises/HAD/Y2020/M10/D23/Exercise.hs
+++ exercises/HAD/Y2020/M10/D23/Exercise.hs
@@ -1,5 +1,7 @@
 module Y2020.M10.D23.Exercise where
 
+import Y2020.M10.D12.Exercise    -- for AirBase
+
 {--
 So, I managed to update country-names in the graph yesterday, and then I
 went to upload the air bases, but got the following error [snippet]:
@@ -21,7 +23,29 @@ THEN: Let's replace the escaped values with unicode. That shouldn't be
 hard, right? It is the 21st century, after all, right?
 
 Actually, ... looking at the raw data, this solution may be easier than
-I anticipated.
+I anticipated. ... But no, changing the String-types to Text-types changes
+the results by not one iota. Oh, well, it was a long detour that was worth
+a try.
+
+So, again, I say unto you, your new task:
+
+Find hard coded \digits and replace with:
+
+ABSOLUTELY NOTHING!
+
+Yay.
 --}
 
+import Data.Map (Map)
+import qualified Data.Text as T    -- hint
 
+stripNonAscii :: Map Icao AirBase -> Map Icao AirBase
+stripNonAscii = undefined
+
+type Index = Int
+
+unicodePoint :: Entity -> Maybe Index
+unicodePoint = undefined    -- returns Just 3, for Chi(e-grave)vres
+
+-- How many AirBases were updated? What did you do for AirBases with multiple
+-- unicode points in their entity-name?
