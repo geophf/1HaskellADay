bebbcade55f1e1f4458f917da4593e95625e033f
geophf <dauclair@hotmail.com>
2020-10-29 20:10:12 -0400

    (*:)-operator and a higher-order alter-function FTW!

--- exercises/HAD/Y2020/M10/D28/Solution.hs
+++ exercises/HAD/Y2020/M10/D28/Solution.hs
@@ -38,7 +38,9 @@ are stored in JSON files in this directory. Read in these JSONs in the data
 structures declared below, then answer the questions following.
 --}
 
-import Y2020.M10.D12.Solution   -- for Country
+import Y2020.M10.D12.Solution hiding (country)   -- for Country
+
+import Control.Arrow ((&&&))
 
 import Data.Aeson
 import Data.Aeson.Types
@@ -46,7 +48,14 @@ import Data.Aeson.Types
 import Data.ByteString.Lazy.Char8 (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as BL
 
+import Data.List (sort)
+import Data.Maybe (fromMaybe)
+
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Set (Set)
+import qualified Data.Set as Set
+
 import Data.Text (Text)
 import qualified Data.Text as T
 
@@ -82,6 +91,8 @@ obj *: label = WD <$> obj .: label <*> obj .: (T.concat [label, "Label"])
 
 -- and with this new operator, we can do this naturally:
 
+type Alliance = Text
+
 data AllianceMember = AllianceMember { alliance :: WikiDatum,
                                        country :: WikiDatum }
    deriving (Eq, Ord, Show)
@@ -114,7 +125,21 @@ WOOT!
 --}
 
 allianceMembers :: ByteString -> [AllianceMember]
-allianceMembers = undefined
+allianceMembers = fromMaybe [] . decode
+
+{--
+>>> BL.readFile (deer ++ alliancesFile)
+...
+>>> let af = it
+>>> let allis = allianceMembers af
+>>> length allis
+42
+>>> head allis
+AllianceMember {alliance = WD {qid = "http://www.wikidata.org/entity/Q7184", 
+                               name = "NATO"},
+                country = WD {qid = "http://www.wikidata.org/entity/Q30", 
+                              name = "United States of America"}}
+--}
 
 -- We're going to have to explore alliances further, as wikidata is woefully
 -- underrepresentative, i.e.:
@@ -125,12 +150,29 @@ allianceMembers = undefined
 -- are mentioned in all the alliances?
 
 countries :: [AllianceMember] -> Set Country
-countries = undefined
+countries = Set.fromList . map (name . country)
+
+{--
+>>> countries allis
+fromList ["Australia","Bulgaria","Canada","Croatia","Denmark","France",
+          "Germany","Italy","Kazakhstan","Kingdom of Denmark","Kyrgyzstan",
+          "Latvia","Montenegro","Netherlands","New Zealand","Norway","Pakistan",
+          "Philippines","Poland","Portugal","Romania","Slovakia","Tajikistan",
+          "Thailand","Turkey","United Kingdom","United States of America",
+          "Uzbekistan"]
+--}
 
 -- How many alliances are there?
 
-alliances :: [AllianceMember] -> Set Country
-alliances = undefined
+alliances :: [AllianceMember] -> Set Alliance
+alliances = Set.fromList . map (name . alliance)
+
+{--
+>>> alliances allis
+fromList ["Anzus","Collective Security Treaty Organisation","Five Eyes","NATO",
+          "Southeast Asia Treaty Organization","Warsaw Pact",
+          "Western European Union","polla"]
+--}
 
 {-- BONUS -------------------------------------------------------
 
@@ -151,18 +193,60 @@ data Opposition = Opposition { alliance1, alliance2 :: WikiDatum }
    deriving Show
 
 instance Eq Opposition where
-   (==) = undefined
+   Opposition a b == Opposition c d =
+      a == c && b == d || a == d && b == c
 
 instance Ord Opposition where
-   compare = undefined       -- for Set insertion
+   compare (Opposition a b) (Opposition c d) = -- compare (a,b) (c,d)
+      -- the above doesn't work: we have to order ab and cd first then compare
+      let ab = sort [a,b]
+          cd = sort [c,d]
+      in compare ab cd
 
 instance FromJSON Opposition where
-   parseJSON = undefined
+   parseJSON = withObject "Opposition" $ \v -> Opposition
+      <$> v *: "alliance1" <*> v *: "alliance2"
 
 oppositions :: ByteString -> Set Opposition
-oppositions = undefined
+oppositions = Set.fromList . fromMaybe [] . decode
 
-weirdOppositions :: Set Opposition -> Set Country
-weirdOppositions = undefined
+{--
+>>> BL.readFile (deer ++ oppositionsFile)
+>>> let ofil = it
+>>> let opz = oppositions ofil
+>>> Set.size opz
+2
+--}
+
+weirdOppositions :: Map Alliance (Set Country) -> Set Opposition -> Set Country
+weirdOppositions m = Set.unions . map (wo' m) . Set.toList
+
+wo' :: Map Alliance (Set Country) -> Opposition -> Set Country
+wo' m (Opposition a b) = Set.intersection (membersOf a) (membersOf b)
+   where membersOf x = fromMaybe Set.empty (Map.lookup (name x) m)
 
 -- returns a set of countries that oppose themselves.
+
+-- but to do weirdness we have to map alliances to countries:
+
+memberCountries :: [AllianceMember] -> Map Alliance (Set Country)
+memberCountries = foldr (uncurry (alter1 f)) Map.empty
+                . map (name . alliance &&& name . country)
+   where f Nothing = Just . Set.singleton
+         f (Just s) = Just . flip Set.insert s
+         alter0 :: Ord k => (Maybe (Set v) -> v -> Maybe (Set v))
+                         -> k -> Map k (Set v) -> v -> Map k (Set v)
+         alter0 f k m0 = maybe m0 (flip (Map.insert k) m0) . f (Map.lookup k m0)
+         alter1 f = flip . alter0 f
+
+{--
+>>> let mc = memberCountries allis
+>>> take 3 $ Map.toList mc
+[("Anzus",fromList ["Australia","New Zealand"]),
+ ("Collective Security Treaty Organisation",fromList ["Kazakhstan","Kyrgyzstan",
+                                                      "Tajikistan","Uzbekistan"]),
+ ("Five Eyes",fromList ["Australia","Canada","New Zealand","United Kingdom"])]
+
+>>> weirdOppositions mc opz
+fromList ["Bulgaria","Poland","Romania"]
+--}
