1e307a65bfbff3a613b4f12c9ed2c475c4d5d33b
geophf <dauclair@hotmail.com>
2020-11-09 00:12:03 -0500

    And, so, now: the Big Kahuna of military alliances of the world.

--- exercises/HAD/Y2020/M11/D10/Exercise.hs
+++ exercises/HAD/Y2020/M11/D10/Exercise.hs
@@ -53,9 +53,17 @@ oasDir = "Y2020/M11/D10/"
 oas :: FilePath
 oas = "org-american-states.wtxt"
 
-stuphfen :: FilePath -> FilePath -> FilePath -> FilePath -> IO AllianceMap
-stuphfen blah blee bleu blau =
-   seedAllianceMap blah blee >>= flip addEU bleu >>= flip addUN blau
+stuphfen :: FilePath -> FilePath -> FilePath -> IO AllianceMap
+stuphfen blah blee blau =
+   seedAllianceMap blah blee >>= flip addUN blau
+
+{--
+>>> stuphfen (dear ++ moderns) (euDir ++ eu) (unDir ++ un)
+...
+>>> let stuf = it
+>>> Map.size stuf
+44
+--}
 
 --- Now you:
 
@@ -64,12 +72,88 @@ parseOAS line = undefined
 
 -- parseOAS, given a line, parses `|{{Country|Barbados}} [... and stuff]`
 
+exOASbon, exOASmal :: String
+exOASbon = concat ["|{{Country|Antigua and Barbuda}} || [[St. John's, Antigua ",
+                   "and Barbuda|St. John's]] || 1981 || 1.78 || $20,977 || ",
+                   "0.780 || [[Eastern Caribbean dollar]] || [[English ",
+                   "language|English]]"]
+
+{--
+>>> parseOAS exOASbon
+Just "Antigua and Barbuda"
+--}
+
+exOASmal = concat ["! Nation !! Capital !! Year<br>joined !! [[List of ",
+                   "countries by GDP (PPP)|GDP (PPP)<br>$Million]] !! [[List of ",
+                   "countries by GDP (PPP) per capita|GDP (PPP)<br>per capita]] ",
+                   "<ref name=\"A1\">{{cite news| first=| last=| title= World ",
+                   "Bank | work= | date= 8 July 2014| url=http://data.worldbank",
+                   ".org/indicator/NY.GDP.PCAP.PP.CD?order=wbapi_data_value_2013",
+                   "+wbapi_data_value+wbapi_data_value-last&sort=desc}}</ref> !! ",
+                   "[[List of countries by Human Development Index|HDI]]<br>",
+                   "<small>2018 est.<br>for 2017</small><ref name=\"HDI\">{{cite ",
+                   "web |url=http://hdr.undp.org/sites/default/files/2018_",
+                   "human_development_statistical_update.pdf |title=2018 Human ",
+                   "Development Report |year=2018 |accessdate=14 September ",
+                   "2018 |publisher=United Nations Development Programme }}",
+                   "</ref> !! [[Currency]] !! [[Official Language|Official<br>",
+                   "language]]s"]
+
+{--                
+>>> parseOAS exOASmal
+Nothing            
+--}
+
 -- which means you can now write:
 
 oasParser :: FilePath -> IO Alliance
 oasParser = undefined
 
+{--
+>>> oasParser (oasDir ++ oas)
+Alliance {name = "Organization of American States", aliases = fromList [], 
+          countries = fromList ["Antigua and Barbuda","Argentina","Bahamas",
+               "Barbados","Belize","Bolivia","Brazil","Canada","Chile",
+               "Colombia","Costa Rica","Cuba","Dominica","Dominican Republic",
+               "Ecuador","El Salvador","Grenada","Guatemala","Guyana","Haiti",
+               "Honduras","Jamaica","Mexico","Nicaragua","Panama","Paraguay",
+               "Peru","Saint Kitts and Nevis","Saint Lucia",
+               "Saint Vincent and the Grenadines","Suriname",
+               "Trinidad and Tobago","United States","Uruguay","Venezuela"]}
+
+>>> let os = it
+>>> Set.size (A.countries os)
+35
+
+$ grep -i "{{Country|" Y2020/M11/D10/org-american-states.wtxt| wc
+      35     848    675
+
+... so that checks out. :D 
+--}
+
 addOAS :: AllianceMap -> FilePath -> IO AllianceMap
 addOAS am merkanFile = undefined
 
+{--
+>>> addOAS stuf (oasDir ++ oas)
+...
+>>> let oses = it
+>>> Map.size oses
+45 
+--}
+
 -- DOIT! TOIT!
+
+-- So, our Big Kahuna for military alliances of the world is definitively:
+
+go :: IO AllianceMap
+go = stuphfen (dear ++ moderns) (euDir ++ eu) (unDir ++ un) >>=
+     flip addOAS (oasDir ++ oas)
+
+{--
+>>> go
+...
+>>> let gone = it
+>>> Map.size gone
+45
+--}
