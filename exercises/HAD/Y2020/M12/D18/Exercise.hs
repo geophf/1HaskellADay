2b4f09e235a76b55b28190b90dbdbc9bf329e2b7
geophf <dauclair@hotmail.com>
2020-12-18 00:33:16 -0500

    unified alliance-fetch function. KMLified results.

--- exercises/HAD/Y2020/M12/D18/Exercise.hs
+++ exercises/HAD/Y2020/M12/D18/Exercise.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Y2020.M12.D17.Exercise where
+module Y2020.M12.D18.Exercise where
 
 {-- if you look at how we upload capitals at ...
 
@@ -34,6 +34,9 @@ We have our work cut out for us.
 --}
 
 import Data.Aeson
+import Data.Aeson.Types (Parser)
+
+import qualified Data.ByteString.Lazy.Char8 as BL
 
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -63,6 +66,27 @@ import Y2020.M12.D16.Solution
 instance FromJSON Capital where
    parseJSON = undefined
 
+{--
+A sample capital returned from the graph-store is:
+
+>>> getGraphResponse url [capitalOfQuery (Country "Malaysia" Nothing [])]
+{"results":[{"columns":["c"],
+             "data":[{"row":[{"name":"Kuala Lumpur",
+                              "location":{"type":"Point",
+                                  "coordinates":[101.695277777,3.147777777],
+                                  "crs":{"srid":4326,"name":"wgs-84",
+                                         "type":"link",
+                                         "properties":{"href":"http://spatialreference.org/ref/epsg/4326/ogcwkt/",               
+                                                 "type":"ogcwkt"}}},
+                              "qid":"http://www.wikidata.org/entity/Q1865"}],
+ "meta":[{"id":1292,"type":"node","deleted":false}]}]}],"errors":[]}
+
+Sheesh.
+--}
+
+convertPoint :: Value -> Parser LongLat
+convertPoint = undefined
+
 -- We also need a query to extract the capital for a given country:
 
 capitalOfQuery :: Country -> Cypher
@@ -70,11 +94,33 @@ capitalOfQuery (Country c _ _) =
    T.concat ["MATCH (:Country { name: \"", c, "\" })-[:CAPITAL]->(c:Capital) ",
              "RETURN c"]
 
-capitalOf :: Endpoint -> Country -> IO Capital
-capitalOf url country = undefined
+-- so, with the capital parser defined above, we get the following
+
+sampleCapital :: String
+sampleCapital =
+   concat ["{\"name\":\"Kuala Lumpur\",",
+             "\"location\":{\"type\":\"Point\",",
+                            "\"coordinates\":[101.695277777,3.147777777],",
+                            "\"crs\":{\"srid\":4326,\"name\":\"wgs-84\",",
+                                      "\"type\":\"link\",",
+                                      "\"properties\":{\"href\":\"http://spatialreference.org/ref/epsg/4326/ogcwkt/\",",
+                                                       "\"type\":\"ogcwkt\"}}},",
+             "\"qid\":\"http://www.wikidata.org/entity/Q1865\"}"]
+
+{--
+>>> (decode $ BL.pack sampleCapital) :: Maybe Capital
+Just (Capital (WD {qid = "http://www.wikidata.org/entity/Q1865",
+                   name = "Kuala Lumpur"})
+               point({ latitude: 3.147777777, longitude: 101.695277777 }))
+--}
+
+fetchCapital :: Endpoint -> Country -> IO (Maybe Capital)
+fetchCapital url country = undefined
 
 {--
 First things first. Let's get our alliance and its countries:
+
+>>> :set -XOverloadedStrings
 
 >>> graphEndpoint
 ...
@@ -87,18 +133,32 @@ First things first. Let's get our alliance and its countries:
 
 So we have a list of countries, but, for an Alliance, we need the Set of
 countries, the create our Alliance
---}
 
 fetchAlliance :: Endpoint -> Name -> IO (Maybe Alliance)
 fetchAlliance url allianceName = undefined
+
+I have `fetchAlliance` commented out, because we can do better. See below.
+--}
 
 -- The below two functions may be useful to getting to the above fetch-function.
 
 fetchAllianceNames :: Endpoint -> Name -> IO [AllianceNames]
 fetchAllianceNames url allianceName = undefined
 
+{--
+>>> fetchAllianceNames url fpda
+[AN "Five Power Defence Arrangements" []]
+--}
+
 fetchAllianceCountries :: Endpoint -> Name -> IO (Set Country)
 fetchAllianceCountries url allianceName = undefined
+
+{--
+>>> fetchAllianceCountries url fpda
+fromList [Country {country = "Australia", ... }]
+
+>>> let ctrs = it
+--}
 
 -- work to getting to the above 2 fetchers are in Y2020.M12.D16.Solution
 
@@ -108,22 +168,35 @@ Just (Alliance {name = "Five Power Defence Arrangements", aliases = fromList [],
                 countries = fromList ["Australia","Malaysia","New Zealand",
                                       "Singapore","United Kingdom"]})
 
-Okay. Great. Now, let's get the capitals of the countries:
+Okay. Great. Now, let's get the capitals of the countries... (the declaration
+is all the way at the top of this module).
 --}
-
-fetchCapital :: Endpoint -> Country -> IO (Maybe Capital)
-fetchCapital url (Country c _ _) = undefined
 
 -- with the country and the capital you can build a CountryInfo value, and
 -- from there, a CountryInfoMap
 
-countryInfoFor :: Country -> Capital -> CountryInfo
+countryInfoFor :: Country -> Maybe Capital -> Maybe CountryInfo
 countryInfoFor = undefined
 
 -- from thence:
 
 fetchCountryInfoMap :: Endpoint -> Set Country -> IO CountryInfoMap
 fetchCountryInfoMap = undefined
+
+{--
+>>> fetchCountryInfoMap url ctrs
+{("Australia",CI {country = WD {qid = "http://www.wikidata.org/entity/Q408",
+                                name = "Australia"},
+                  capital = WD {qid = "http://www.wikidata.org/entity/Q3114",
+                                name = "Canberra"},
+                  latLong = point({ latitude: -35.293055555, longitude: 149.126944444 })}),...}
+
+With the above functions we can now use the set of countries to return an
+alliance along with the CountryInfoMap.
+--}
+
+fetchAllianceInfo :: Endpoint -> Name -> IO (Maybe (Alliance, CountryInfoMap))
+fetchAllianceInfo url allianceName = undefined
 
 {-- BONUS -------------------------------------------------------
 
