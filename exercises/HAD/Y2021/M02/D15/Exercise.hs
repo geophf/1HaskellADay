1c9fe93d9a2d6c400610cb434d5bbe094ee6bf41
geophf <dauclair@hotmail.com>
2021-02-13 03:58:20 -0500

    I've got an idea! Let's use the COMPUTER to figure out where the JSON file is broken!

---
+++ exercises/HAD/Y2021/M02/D15/Exercise.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Y2021.M02.D15.Exercise where
+
+{--
+Look at this directory structure!
+
+wine-reviews-aa-aa-aa-aa
+wine-reviews-aa-aa-aa-ab-aa-aa
+wine-reviews-aa-aa-aa-ab-aa-ab-aa-aa
+... 60 more files ...
+wine-reviews-ai-ab-ab
+wine-reviews-aj
+wine-reviews-ak
+
+This is me, trying to hand-parse-out where embedded newlines broke a JSON
+file of wine-reviews.
+
+There has to be a better way than this slog!
+
+Today's Haskell problem: ingest a biggish JSON file (~800k lines) then
+show line numbers and lines where embedded newlines break the file.
+--}
+
+import qualified Y2021.M02.D03.Solution as WR
+
+import Data.ByteString.Lazy.Char8 (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+type LineNumber = Integer
+type Line = ByteString
+
+illegalJSON :: FilePath -> IO [(LineNumber, Line)]
+illegalJSON = undefined
+
+{-- BONUS ------------------------------------------------------------------
+
+Now that you've identified the problem areas, ... fix the file.
+--}
+
+repairJSON :: FilePath -> FilePath -> IO ()
+repairJSON brokenFileIn fixedFileOut = undefined
