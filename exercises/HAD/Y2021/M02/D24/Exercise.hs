ee105a34f22ecae65b2dd696605947dae8199f96
geophf <dauclair@hotmail.com>
2021-02-24 11:40:49 -0500

    "What's today's date?" is today's #haskell problem.

---
+++ exercises/HAD/Y2021/M02/D24/Exercise.hs
@@ -0,0 +1,15 @@
+module Y2021.M02.D24.Exercise where
+
+import Data.Time
+
+{--
+What's today's date?
+
+Today's Haskell problem is to get the current date as a YYYY-mm-dd-formatted
+string from ... wherever you get the current date.
+--}
+
+today :: IO String
+today = undefined
+
+-- go get'm!
