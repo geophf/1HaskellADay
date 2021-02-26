98948bb1abefd7b3850977f0f6548ad9929a573f
geophf <dauclair@hotmail.com>
2021-02-26 14:46:01 -0500

    Simplified the curl-command to save results to absolute path.

--- exercises/HAD/CryptoCoin/CoinMarketCap/curl-command.sh
+++ exercises/HAD/CryptoCoin/CoinMarketCap/curl-command.sh
@@ -1,3 +1,3 @@
 LE_DATE=$(date +%Y-%m-%d)
 
-curl -H "X-CMC_PRO_API_KEY: $COIN_MARKET_CAP_API_KEY" -H "Accept: application/json" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/map > rankings/2021/coins-$LE_DATE.json
+cd $COIN_MARKET_CAP_DIR; curl -H "X-CMC_PRO_API_KEY: $COIN_MARKET_CAP_API_KEY" -H "Accept: application/json" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/map > rankings/2021/coins-$LE_DATE.json
