d77c5516fbe36b46376bc7c16a4400ede7b3fd9d
geophf <dauclair@hotmail.com>
2021-03-05 05:44:31 -0500

    Today we're shell-scripting ... WITH HASKELL!

---
+++ exercises/HAD/Y2021/M03/D05/im.hs
@@ -0,0 +1,8 @@
+import System.Environment (getArgs)
+
+-- This is the application that calls 'greet' to wish our User well today.
+
+import Y2021.M03.D05.Exercise (greet)
+
+main :: IO ()
+main = undefined
