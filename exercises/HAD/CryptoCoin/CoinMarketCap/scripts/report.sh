f46c649e5dbb86bfc3cdfea97dbca08a2332b06c
geophf <dauclair@hotmail.com>
2021-03-09 04:28:57 -0500

    Added polling of listings, now, as well as rankings for e-coins.

--- exercises/HAD/CryptoCoin/CoinMarketCap/scripts/report.sh
+++ exercises/HAD/CryptoCoin/CoinMarketCap/scripts/report.sh
@@ -1,9 +1,15 @@
 LE_DATE=$(date +%Y-%m-%d)
 
-OUT_FILE=$COIN_MARKET_CAP_DIR/rankings/2021/coins-$LE_DATE.json
+RANKING_FILE=$COIN_MARKET_CAP_DIR/rankings/2021/coins-$LE_DATE.json
+LISTING_FILE=$COIN_MARKET_CAP_DIR/listings/2021/quots-$LE_DATE.json
+
+LIST_CMD=cryptocurrency/listings/latest?start=1&limit=5000&convert=USD
+
+CURL_CMD=$COIN_MARKET_CAP_DIR/scripts/curl-command.sh
 
 cd $COIN_MARKET_CAP_DIR/scripts
-$COIN_MARKET_CAP_DIR/scripts/curl-command.sh cryptocurrency/map $OUT_FILE
+$CURL_CMD cryptocurrency/map $RANKING_FILE
+$CURL_CMD $LIST_CMD $LISTING_FILE
 $COIN_MARKET_CAP_DIR/scripts/ranker
 
 ghc ranker.hs
