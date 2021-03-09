b0f8d41f6e6de38229a530f6cb59ad4a77aefa87
geophf <dauclair@hotmail.com>
2021-03-09 14:29:37 -0500

    Quoted out '&'s for GET request.

--- exercises/HAD/CryptoCoin/CoinMarketCap/scripts/curl-command.sh
+++ exercises/HAD/CryptoCoin/CoinMarketCap/scripts/curl-command.sh
@@ -5,6 +5,6 @@ ENDPOINT=https://pro-api.coinmarketcap.com/v1
 cd $COIN_MARKET_CAP_DIR; \
 curl -H "X-CMC_PRO_API_KEY: $COIN_MARKET_CAP_API_KEY" \
      -H "Accept: application/json" \
-     -G  $ENDPOINT/$1 > $OUT_FILE
+     -G $ENDPOINT/$1 > $OUT_FILE
 
 echo "Wrote latest rankings to $OUT_FILE."
--- exercises/HAD/CryptoCoin/CoinMarketCap/scripts/report.sh
+++ exercises/HAD/CryptoCoin/CoinMarketCap/scripts/report.sh
@@ -3,18 +3,18 @@ LE_DATE=$(date +%Y-%m-%d)
 RANKING_FILE=$COIN_MARKET_CAP_DIR/rankings/2021/coins-$LE_DATE.json
 LISTING_FILE=$COIN_MARKET_CAP_DIR/listings/2021/quots-$LE_DATE.json
 
-LIST_CMD=cryptocurrency/listings/latest?start=1&limit=5000&convert=USD
+LIST_CMD="listings/latest?start=1&limit=5000&convert=USD"
 
 CURL_CMD=$COIN_MARKET_CAP_DIR/scripts/curl-command.sh
 
 cd $COIN_MARKET_CAP_DIR/scripts
-$CURL_CMD cryptocurrency/map $RANKING_FILE
-$CURL_CMD $LIST_CMD $LISTING_FILE
-$COIN_MARKET_CAP_DIR/scripts/ranker
+# $CURL_CMD cryptocurrency/map $RANKING_FILE
+$CURL_CMD cryptocurrency/$LIST_CMD $LISTING_FILE
+# $COIN_MARKET_CAP_DIR/scripts/ranker
 
-ghc ranker.hs
-ghc report.hs
+# ghc ranker.hs
+# ghc report.hs
 
-$COIN_MARKET_CAP_DIR/scripts/report
+# $COIN_MARKET_CAP_DIR/scripts/report
 
 echo "done."
