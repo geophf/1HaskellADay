fe6282de5b46feaeb67f02ffba0cc0d68e18af3e
geophf <dauclair@hotmail.com>
2021-03-19 10:43:21 -0400

    pi. Not pie. Not tau. pi... you know, or: close enough.

---
+++ exercises/HAD/Y2021/M03/D19/Exercise.hs
@@ -0,0 +1,31 @@
+module Y2021.M03.D19.Exercise where
+
+{--
+@AnecdotesMaths has been publishing a formula for pi, or to approximate pi,
+every day this month. I've been retweeting those formulae, because I like pi.
+
+I also like tau, and I also like cake, so there's that.
+
+Take one of those formulae and compute pi to a 'reasonable' degree of accuracy.
+
+Print pi.
+--}
+
+pi :: Double -- or some other numerical representation that suits you.
+pi = undefined
+
+{-- BONUS -------------------------------------------------------
+
+The above formula is either inaccurate or does not terminate ... or both.
+
+Have a formula that either stops after the nth iteration or stops within a
+certain degree of accuracy.
+--}
+
+nthPi :: Int -> Double
+nthPi n = undefined
+
+piWithin :: Double -> Double
+piWithin epsilon = undefined
+
+-- n.b. Today, 3.19 is a 'kinda'-accurate representation of pi? maybe?
