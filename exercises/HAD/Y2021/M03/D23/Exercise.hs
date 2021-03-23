2d1898c2249f6478a50e9e199d8b8f753726590d
geophf <dauclair@hotmail.com>
2021-03-23 12:13:16 -0400

    Let's go FORTH! and conquer!

---
+++ exercises/HAD/Y2021/M03/D23/Exercise.hs
@@ -0,0 +1,45 @@
+module Y2021.M03.D23.Exercise where
+
+{-- 
+HOWDY! So, what shall we do today?
+
+I would like to implement FORTH, but that's just me. Did you know that in
+FORTH pi is simply defined as:
+
+: pi 355 113 */ ;
+
+I LOVE IT! What's even more amazing is that Chuck Moore developed Forth
+as a telescope guidance control language, and that definition of pi got the
+telescope to point to any star. 355 113 */ was accurate enough for astronomy!
+
+YAY!
+
+Okay, so: let's do this.
+
+The FORTH prompt is ('was'? 'is'?) "OK> " and you enter your FORTH and you
+fly.
+
+A number is pushed on the stack and an operator reads from the stack.
+
+Let's implement an RPN with * / + - and . where '.' prints pops the stack and
+prints that value.
+--}
+
+import Control.Monad.State
+
+data Stack = Stack [Int]
+   deriving (Eq, Ord, Show)
+
+forth :: String -> StateT Stack IO ()
+forth = undefined
+
+repl :: IO ()   -- for "Read Evaluate Print Loop"
+repl = undefined
+
+{--
+>>> repl
+OK> 3 4 + .
+7
+
+OK> 
+--}
