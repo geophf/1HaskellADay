c03534f38d9ccb77fa594a65305eb2ac47e13de1
geophf <dauclair@hotmail.com>
2021-03-24 05:24:34 -0400

    Added nextIndex-function.

--- exercises/HAD/Data/LookupTable.hs
+++ exercises/HAD/Data/LookupTable.hs
@@ -4,6 +4,9 @@ module Data.LookupTable where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
+
+import qualified Data.Set as Set
+
 import Data.Tuple (swap)
 
 type LookupTable = Map String Integer
@@ -14,3 +17,15 @@ type LookDown = Map Integer String
 
 lookdown :: LookupTable -> LookDown
 lookdown = Map.fromList . map swap . Map.toList
+
+-- but what's the next index?
+
+nextIndex :: LookupTable -> Integer
+nextIndex = succ . maybe 0 fst . Set.maxView . Map.keysSet . lookdown
+
+{--
+>>> nextIndex Map.empty
+1
+
+BOOM!
+--}
