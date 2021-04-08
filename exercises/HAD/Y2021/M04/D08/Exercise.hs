3b8b3a99ee62189e70f23dbe1a9c4385c72adc06
geophf <dauclair@hotmail.com>
2021-04-08 13:43:02 -0400

    Optionally parsing a string into some value of some type. Some how.

---
+++ exercises/HAD/Y2021/M04/D08/Exercise.hs
@@ -0,0 +1,36 @@
+module Y2021.M04.D08.Exercise where
+
+{--
+readMaybe.
+
+You have a String that you'd like to parse into a typed value, ...
+
+... because this is Haskell, and all, ICYMI, ...
+
+But you're not sure the string is well-formed.
+
+What you want to do is accept the string if it is well-formed, and reject
+it if it isn't, and move onto the next task, that is to say.
+
+>>> (read "5") :: Int
+5
+
+works, but 
+
+>>> (read "5s") :: Int
+*** Exception: Prelude.read: no parse
+
+... we don't want to interrupt our program, nor our accumulated value-set,
+we want to move onto the next values and work with what we have.
+--}
+
+sumnums :: String
+sumnums = "1 3 4 3 5 6 4 6 4 g 4  6 3 2 5  2 2 d 3 6"
+
+-- what is the sum of the numbers in the above input string?
+
+readMaybe :: String -> Maybe a
+readMaybe = undefined
+
+-- and how would you parse the above string into individual substrings for
+-- parsing?
