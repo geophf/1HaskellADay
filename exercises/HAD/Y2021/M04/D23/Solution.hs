c6ef544182bae1ba941e65c1857619a45df264e8
geophf <dauclair@hotmail.com>
2021-04-23 15:17:55 -0400

    The winner of the Viking-Samurai deth-match is: EL GEOPHF!

---
+++ exercises/HAD/Y2021/M04/D23/Solution.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Y2021.M04.D23.Solution where
+
+{--
+There are so many ways Haskell and the 'real world' interface.
+
+We've been looking at how (sometimes dirty) data intersects with Haskell-typed
+values.
+
+Here's an interesting 'real world'-problem.
+
+Sometimes somebody factors data differently than you do. For example, wikidata's
+geolocation as JSON is ... 'interesting.' So is neo4j's.
+
+But that's why I have the LongLat-library. Ugh.
+
+I recently came up with another example, and it can be stated thus:
+
+The ability for a fighter to win a battle is based upon both Kung Fu 
+fighting-style (and skill), and the foods eaten. These are two different
+things, but they are both known as Skill
+--}
+
+import Data.Maybe (mapMaybe)
+
+import Y2021.M04.D08.Solution (readMaybe)
+
+data Skill = FS FightingStyle | FE FoodEaten
+   deriving (Eq, Ord, Show)
+
+data FightingStyle = Karate | KravMaga | TaeKwonDo | Savate | MuayThai | Arnis
+   deriving (Eq, Ord, Show, Read)
+
+-- https://medium.com/@krissturmey/the-diets-of-historys-greatest-warriors-4d7439c0b6bd
+
+data FoodEaten = Fish | Meat | Barley | Rye | Oats | Milk | Eggs
+   deriving (Eq, Ord, Show, Read)
+
+-- and we have the complexity of Veggies and Fruits as multiple values
+-- of a FoodEaten-variant, but I'm going to ignore that complexity for now.
+
+{--
+So, here's the thing: these skills are stored in two tables in a data-store,
+but one table indicates kind (food eaten or fighting style), and the other
+table has an admixture of both fighting-style and food-eaten values.
+
+How do you read a value, given this unexpected factoring?
+--}
+
+readSkill :: String -> String -> Maybe Skill
+readSkill (despace -> fsfe) = flip withKind fsfe
+
+{--
+readSkill takes a fighting-style or a food-eaten value-as-string, then it takes
+the kind-as-string and returns a Skill-value, if possible.
+--}
+
+-- convert the below data into Skill values
+
+warrior :: [(String, String)]
+warrior =
+   zip ["Krav Maga", "Barley", "Rye", "Milk", "Muay Thai"]
+       ["Fighting Style", "Food Eaten", "Food Eaten", "Food Eaten", "Fighting Style"]
+
+-- you'll also note that the data values are stored in manager-readable format
+-- fix the values before parsing them.
+
+-- we want to use the kind to set the context
+
+withKind :: String -> String -> Maybe Skill
+withKind "Fighting Style" fstyl = FS <$> readMaybe fstyl
+withKind "Food Eaten" foodage = FE <$> readMaybe foodage
+
+despace :: String -> String
+despace = concat . words
+
+{--
+>>> mapMaybe (uncurry readSkill) warrior 
+[FS KravMaga,FE Barley,FE Rye,FE Milk,FS MuayThai]
+--}
