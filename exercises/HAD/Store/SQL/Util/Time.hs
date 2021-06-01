eb01ab593f6bee70895ce275cf7ae3b44949ac00
geophf <dauclair@hotmail.com>
2021-06-01 15:11:45 -0400

    Added a function to get the latest date from a data-table.

--- exercises/HAD/Store/SQL/Util/Time.hs
+++ exercises/HAD/Store/SQL/Util/Time.hs
@@ -2,10 +2,15 @@ module Store.SQL.Util.Time where
 
 -- converts SQL dates to Haskell days
 
+import qualified Data.ByteString.Char8 as B
 import Data.Time
 import Data.Time.Clock
 
+import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.Time
+import Database.PostgreSQL.Simple.Types (Query(Query))
+
+import Store.SQL.Util.TaggedTypes (TaggedType, untag)
 
 d2d :: LocalTimestamp -> Maybe Day
 d2d (Finite d) = Just (localDay d)
@@ -17,3 +22,17 @@ utc2d _          = Nothing
 
 d2utc :: Day -> UTCTimestamp
 d2utc = Finite . flip UTCTime (secondsToDiffTime 0)
+
+-- We need to get the most recent day from a data-table
+
+type TableName = String
+type ColumnName = String
+
+latest :: Connection -> TableName -> ColumnName -> IO Day
+latest conn tableName dateColumn =
+   let query = Query (B.pack ("SELECT max(" ++ dateColumn ++ ") FROM "
+                     ++ tableName)) in
+   untag <$> tday conn query
+
+tday :: Connection -> Query -> IO (TaggedType Day)
+tday conn qury = head <$> query_ conn qury
